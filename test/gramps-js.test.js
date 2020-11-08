715991a75a0fe09e1180998991ca5727bb858ea9
David Straub <straub@protonmail.com>
2020-11-08 11:45:55 +0100

    Change GrampsJs.js content

--- test/gramps-js.test.js
+++ test/gramps-js.test.js
@@ -13,7 +13,7 @@ describe('GrampsJs', () => {
   it('renders a h1', () => {
     const h1 = element.shadowRoot.querySelector('h1');
     expect(h1).to.exist;
-    expect(h1.textContent).to.equal('My app');
+    expect(h1.textContent).to.equal('Gramps.js');
   });
 
   it('passes the a11y audit', async () => {
