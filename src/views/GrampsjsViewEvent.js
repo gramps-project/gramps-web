fa3e12a7d5220a14c374495ce5d79537cad53778
David Straub <straub@protonmail.com>
2020-11-17 19:09:39 +0100

    Add event and events view

---
+++ src/views/GrampsjsViewEvent.js
@@ -0,0 +1,70 @@
+import { html, css } from 'lit-element';
+
+import { GrampsjsView } from './GrampsjsView.js'
+import { apiGet } from '../api.js'
+
+
+export class GrampsjsViewEvent extends GrampsjsView {
+  static get styles() {
+    return [
+      super.styles,
+      css`
+      :host {
+      }
+    `];
+  }
+
+
+  static get properties() {
+    return {
+      grampsId: { type: String },
+      _data: { type: Object },
+    };
+  }
+
+
+  constructor() {
+    super();
+    this._data = {};
+  }
+
+
+  renderContent() {
+    if (Object.keys(this._data).length === 0) {
+      if (this.loading) {
+        return html``
+      }
+      return html``
+    }
+    return html`
+    Event view ${this.grampsId}
+    `;
+
+  }
+
+  update(changed) {
+    super.update(changed);
+    if (changed.has('grampsId')) {
+        this._updateData()
+    }
+  }
+
+  _updateData() {
+    if (this.grampsId !== undefined && this.grampsId) {
+      this._data = {}
+      this.loading = true
+      apiGet(`/api/events/?gramps_id=${this.grampsId}&profile`).then(data => {
+        this.loading = false;
+        if ('data' in data) {
+          [this._data] = data.data;
+        } else if ('error' in data) {
+          this.error = true
+          this._errorMessage = data.error
+        }
+      })
+    }
+  }
+}
+
+
+window.customElements.define('grampsjs-view-event', GrampsjsViewEvent);
---
+++ src/views/GrampsjsViewEvents.js
@@ -0,0 +1,100 @@
+/*
+Events list view
+*/
+
+
+import { html, css } from 'lit-element';
+
+import '@vaadin/vaadin-grid/theme/material/vaadin-grid.js'
+
+import { GrampsjsView } from './GrampsjsView.js'
+import { apiGet } from '../api.js'
+
+
+export class GrampsjsViewEvents extends GrampsjsView {
+  static get styles() {
+    return [
+      super.styles,
+      css`
+      :host {
+      }
+
+      vaadin-grid {
+        max-height: calc(100vh - 190px);
+      }
+
+      vaadin-grid {
+        font-family: Roboto;
+        font-weight: 300;
+      }
+    `];
+  }
+
+
+  static get properties() {
+    return {
+      _data: { type: Array },
+    };
+  }
+
+
+  constructor() {
+    super();
+    this._data = [];
+  }
+
+
+  renderContent() {
+    if (this._data.length === 0) {
+      return html``
+    }
+    return html`
+    <vaadin-grid .items="${this._data}" @click="${this._handleGridClick}">
+      <vaadin-grid-column path="grampsId" header="${this._('Gramps ID')}"></vaadin-grid-column>
+      <vaadin-grid-column path="type" header="${this._('Event Type')}"></vaadin-grid-column>
+      <vaadin-grid-column path="date" header="${this._('Date')}"></vaadin-grid-column>
+      <vaadin-grid-column path="place" header="${this._('Place')}"></vaadin-grid-column>
+    </vaadin-grid>
+    `;
+
+  }
+
+  firstUpdated() {
+    this._fetchData()
+  }
+
+  _handleGridClick(e) {
+    const grid = e.currentTarget;
+    const {item} = grid.getEventContext(e);
+    if (item.grampsId){
+      this.dispatchEvent(new CustomEvent('nav', {bubbles: true, composed: true, detail: {path: `event/${item.grampsId}`}}));
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _formatRow(row) {
+    const formattedRow = {
+      grampsId: row.gramps_id,
+      type: row?.profile?.type,
+      date: row?.profile?.date,
+      place: row?.profile?.place
+    }
+    return formattedRow
+  }
+
+  _fetchData() {
+    this.loading = true;
+    apiGet(`/api/events/?profile&keys=gramps_id,profile`).then(data => {
+      this.loading = false;
+      if ('data' in data) {
+        this._data = data.data.map((row) => this._formatRow(row))
+      } else if ('error' in data) {
+        this.error = true
+        this._errorMessage = data.error
+      }
+    })
+  }
+}
+
+
+window.customElements.define('grampsjs-view-events', GrampsjsViewEvents);
