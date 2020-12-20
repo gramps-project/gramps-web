58e7f8d3de9700d26398f50bea6b11df84a6d123
David Straub <straub@protonmail.com>
2020-12-20 20:30:00 +0100

    Rollup: copy leaflet CSS

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,21 @@
 import merge from 'deepmerge'
+import copy from 'rollup-plugin-copy'
 import {createSpaConfig} from '@open-wc/building-rollup'
+
 
 const baseConfig = createSpaConfig({
   developmentMode: process.env.ROLLUP_WATCH === 'true',
   injectServiceWorker: true,
 })
 
+
 export default merge(baseConfig, {
   input: './index.html',
+  plugins: [
+    copy({
+      targets: [
+        {src: './leaflet.css', dest: 'dist/'},
+      ]
+    })
+  ]
 })
