45e8c00b9b7d4c3219ca3dcfedafa200de589ff1
David Straub <straub@protonmail.com>
2020-12-21 17:50:29 +0100

    Change implementation of set home person

--- src/views/GrampsjsViewSettingsOnboarding.js
+++ src/views/GrampsjsViewSettingsOnboarding.js
@@ -2,9 +2,10 @@ import {html, css} from 'lit-element'
 
 import {GrampsjsView} from './GrampsjsView.js'
 import {apiGet, getSettings, updateSettings} from '../api.js'
+import {debounce} from '../util.js'
 import '@material/mwc-textfield'
 import '@material/mwc-button'
-import '@material/mwc-select'
+import '@material/mwc-menu'
 
 
 export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
@@ -17,13 +18,18 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
         --mdc-theme-primary: #C62828;
       }
 
-      mwc-select {
+      mwc-select, mwc-textfield {
         width: 100%;
         max-width: 30em;
+        margin-bottom: 10px;
       }
 
       #complete-button {
         margin-top: 25px;
+      }
+
+      #home-person-menu {
+        max-height: 5em;
       }
     `]
   }
@@ -73,12 +79,7 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
   }
 
   _isCompleted() {
-    const personSelect = this.shadowRoot.getElementById('select-person')
-    const langSelect = this.shadowRoot.getElementById('select-language')
-    if (!personSelect || !langSelect) {
-      return false
-    }
-    if (personSelect.value && langSelect.value) {
+    if (this._settings.homePerson && this._settings.lang) {
       return true
     }
     return false
@@ -91,26 +92,44 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
 
   renderPersonSelect() {
     return html`
-    <mwc-select
-      id="select-person"
-      label="${this._peopleLoading ? this._('Loading...') : this._('Home Person')}"
-      @selected="${this._handlePersonSelected}"
-      ?disabled="${this._peopleLoading}">
-    ${this._people.map(obj => html`
-      <mwc-list-item
-        value="${obj.gramps_id}"
-        ?selected="${obj.gramps_id === this._settings.homePerson}"
-        >${obj.string}</mwc-list-item>
-      `, this)}
-    </mwc-select>
+    <div style="position: relative;">
+      <mwc-textfield
+        iconTrailing="keyboard"
+        id="select-person"
+        label="${this._('Select...')}"
+        @input="${debounce(() => this._fetchDataPeople(), 500)}"
+        >
+      </mwc-textfield>
+      <p>${this._settings.homePerson && this._people.length ?
+    `${this._('Selected')}: ${this._people.filter(obj => obj.gramps_id === this._settings.homePerson)[0].string}` : ''}
+      </p>
+      <mwc-menu id="home-person-menu" style="position: relative;">
+      ${this._people.map(obj => html`
+        <mwc-list-item
+          value="${obj.gramps_id}"
+          @click="${() => this._handlePersonSelected(obj.gramps_id)}"
+          >${obj.string}</mwc-list-item>
+        `, this)}
+      </mwc-menu>
+    </div>
   `
   }
+
+  _openMenu() {
+    const textField = this.shadowRoot.getElementById('select-person')
+    if (textField.value && this._people.length) {
+      const menu = this.shadowRoot.getElementById('home-person-menu')
+      menu.anchor = textField
+      menu.open = true
+    }
+  }
+
 
   renderLangSelect() {
     return html`
     <mwc-select
       id="select-language"
-      label="${this._langLoading ? this._('Loading...') : this._('Language')}"
+      label="${this._langLoading ? this._('Loading items...') : this._('Language')}"
       @selected="${this._handleLangSelected}"
       ?disabled="${this._langLoading}">
     ${Object.keys(this._translations).map(key => html`
@@ -132,10 +151,8 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
     }
   }
 
-  _handlePersonSelected(event) {
-    const i = event.detail.index
-    if (i !== null && i !== undefined) {
-      const grampsId = this._people[i].gramps_id
+  _handlePersonSelected(grampsId) {
+    if (grampsId) {
       updateSettings({homePerson: grampsId})
       this._handleStorage()
     }
@@ -153,22 +170,25 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
 
   firstUpdated() {
     if (this.active) {
-      this._fetchData()
+      this._fetchDataLang()
+      this._fetchDataPeople()
     }
+    const textField = this.shadowRoot.getElementById('select-person')
+    const menu = this.shadowRoot.getElementById('home-person-menu')
+    menu.anchor = textField
   }
 
   update(changed) {
     super.update(changed)
     if (changed.has('active') && this.active) {
-      if(!this.loading && this._people.length === 0) {
-        this._fetchData()
+      if(!this._langLoading && Object.keys(this._translations).length === 0) {
+        this._fetchDataLang()
       }
     }
   }
 
-  async _fetchData() {
+  async _fetchDataLang() {
     this.loading = true
-    this._peopleLoading = true
     this._langLoading = true
     const dataTrans = await apiGet('/api/translations/')
     if ('data' in dataTrans) {
@@ -179,19 +199,32 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
       return
     }
     this._langLoading = false
-    const dataPeople = await apiGet('/api/people/?profile=self&keys=gramps_id,profile')
+    if (!this._peopleLoading) {
+      this.loading = false
+    }
+  }
+
+  async _fetchDataPeople() {
+    const textField = this.shadowRoot.getElementById('select-person')
+    this.loading = true
+    this._peopleLoading = true
+    const seachPhrase = this._settings.homePerson? `(${textField.value} OR ${this._settings.homePerson})` : textField.value
+    const dataPeople = await apiGet(`/api/search/?profile=self&query=${encodeURIComponent(`${seachPhrase} AND type:person)`)}`)
     if ('data' in dataPeople) {
       this._people = dataPeople.data.map(obj =>({
-        gramps_id: obj.gramps_id,
-        string: `${obj.profile.name_given || '...'} ${obj.profile.name_surname || '...'}${obj.profile?.birth?.date ? ` * ${obj.profile?.birth?.date}` : ''}`
+        gramps_id: obj.object.gramps_id,
+        string: `${obj.object.profile.name_given || '...'} ${obj.object.profile.name_surname || '...'}${obj.object.profile?.birth?.date ? ` * ${obj.object.profile?.birth?.date}` : ''}`
       })).sort((a, b) => a.string > b.string)
     } else if ('error' in dataPeople) {
       this.error = true
       this._errorMessage = dataPeople.error
       return
     }
+    this._openMenu()
     this._peopleLoading = false
-    this.loading = false
+    if (!this._langLoading) {
+      this.loading = false
+    }
   }
 
 
