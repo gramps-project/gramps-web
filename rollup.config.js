916cbe9d9a04858a90bc71f6bbc9723d3b1e7cb8
David Straub <straub@protonmail.com>
2021-01-02 14:45:39 +0100

    [rollup] Add BASE_DIR environment variable

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,31 @@ import copy from 'rollup-plugin-copy'
 import {createSpaConfig} from '@open-wc/building-rollup'
 import replace from '@rollup/plugin-replace'
 
-const baseConfig = createSpaConfig({
-  developmentMode: process.env.ROLLUP_WATCH === 'true',
-  injectServiceWorker: true,
-})
-
 
 const API_URL = (process.env.API_URL === undefined)
   ? ''
   : process.env.API_URL
+
+
+const BASE_DIR = (process.env.BASE_DIR === undefined)
+  ? ''
+  : process.env.BASE_DIR
+
+
+const baseConfig = createSpaConfig({
+  developmentMode: process.env.ROLLUP_WATCH === 'true',
+  injectServiceWorker: true,
+  html: {
+    transform: [
+      (html) =>
+        html.replace(
+          '<base href="/">',
+          `<base href="/${BASE_DIR}">`
+        ),
+    ],
+  },
+})
+
 
 
 export default merge(baseConfig, {
