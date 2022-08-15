9eaa41528a642974534dda0c4715d7d0f9ba08f8
Yusuf Khasbulatov <khashashin@gmail.com>
2022-08-15 16:07:14 +0200

    Added some plugins to handle json translation files. Rollup now converts them to javascript objects during application build

--- src/lang/index.js
+++ src/lang/index.js
@@ -2,7 +2,6 @@ import en from './en_GB.json' assert {type: 'json'}
 import ru from './ru.json' assert {type: 'json'}
 import de from './de.json' assert {type: 'json'}
 
-
 const languages = {
   en: en,
   ru: ru,
