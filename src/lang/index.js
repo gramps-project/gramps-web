c19e8211d8ceeaa9be8c64131e99e36c153744f5
Yusuf Khasbulatov <khashashin@gmail.com>
2022-08-15 14:10:15 +0200

    add index file to handle exporting all translations so we can import them as a single line in string.js

---
+++ src/lang/index.js
@@ -0,0 +1,14 @@
+import en from './en_GB.json' assert {type: 'json'}
+import ru from './ru.json' assert {type: 'json'}
+import de from './de.json' assert {type: 'json'}
+
+
+const languages = {
+  en: en,
+  ru: ru,
+  de: de
+}
+
+const translations = languages
+
+export default translations
