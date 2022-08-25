d6308b914227ea1f2eee1f1e805b05525a0eff0b
David Straub <straub@protonmail.com>
2022-08-25 08:48:32 +0200

    Add new languages (#41)

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,11 +1,17 @@
-import en from './en_GB.json' assert {type: 'json'}
+import en from './en.json' assert {type: 'json'}
 import ru from './ru.json' assert {type: 'json'}
 import de from './de.json' assert {type: 'json'}
+import en_GB from './en_GB.json' assert {type: 'json'}
+import pt_PT from './pt_PT.json' assert {type: 'json'}
+import nb from './nb.json' assert {type: 'json'}
 
 const languages = {
   en: en,
   ru: ru,
-  de: de
+  de: de,
+  en_GB: en_GB,
+  pt_PT: pt_PT,
+  nb: nb
 }
 
 const translations = languages
