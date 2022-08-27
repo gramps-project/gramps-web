4dd9ab2ae4cd82117a0cb74e1a59f8180861f52f
David Straub <straub@protonmail.com>
2022-08-27 15:11:20 +0200

    Filter search results by object type (fixes #62) (#89)
    * Implement toggle buttons in search view
    
    * Search based on filters
    
    * Add strings
    
    * Exclude tag type
    
    * Add strings to en

---
+++ src/components/GrampsjsButtonToggle.js
@@ -0,0 +1,56 @@
+import {LitElement, css, html} from 'lit'
+import '@material/mwc-button'
+import '@material/mwc-icon'
+
+import {sharedStyles} from '../SharedStyles.js'
+import {GrampsjsTranslateMixin} from '../mixins/GrampsjsTranslateMixin.js'
+import {fireEvent} from '../util.js'
+
+export class GrampsjsButtonToggle extends GrampsjsTranslateMixin(LitElement) {
+  static get styles () {
+    return [
+      sharedStyles,
+      css`
+      mwc-button {
+      --mdc-typography-button-text-transform: none;
+      --mdc-typography-button-font-weight: 400;
+      --mdc-typography-button-letter-spacing: 0px;
+      --mdc-typography-button-font-size: 13px;
+      }
+      `
+    ]
+  }
+
+  static get properties () {
+    return {
+      icon: {type: String},
+      checked: {type: Boolean}
+    }
+  }
+
+  constructor () {
+    super()
+    this.icon = ''
+    this.checked = false
+  }
+
+  render () {
+    return html`
+    <mwc-button
+      dense
+      ?unelevated="${this.checked}"
+      icon="${this.icon}"
+      @click="${this.toggle}"
+    >
+      <slot></slot>
+    </mwc-button>
+    `
+  }
+
+  toggle () {
+    this.checked = !this.checked
+    fireEvent(this, 'grampsjs-button-toggle:toggle', {checked: this.checked})
+  }
+}
+
+window.customElements.define('grampsjs-button-toggle', GrampsjsButtonToggle)
