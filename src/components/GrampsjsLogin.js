7e435d45121a2999c8020f044699401cd5b3adf1
David Straub <straub@protonmail.com>
2022-08-27 10:04:06 +0200

    Localize login page

--- src/components/GrampsjsLogin.js
+++ src/components/GrampsjsLogin.js
@@ -1,274 +1,369 @@
+import { html, css, LitElement } from 'lit';
 
-import {html, css, LitElement} from 'lit'
+import '@material/mwc-icon';
+import '@material/mwc-button';
+import '@material/mwc-textfield';
+import '@material/mwc-circular-progress';
 
-import '@material/mwc-icon'
-import '@material/mwc-button'
-import '@material/mwc-textfield'
-import '@material/mwc-circular-progress'
+import './GrampsjsPasswordManagerPolyfill';
+import { sharedStyles } from '../SharedStyles.js';
+import { apiGetTokens, apiResetPassword, apiRegisterUser } from '../api.js';
+import { fireEvent } from '../util.js';
+import { GrampsjsTranslateMixin } from '../mixins/GrampsjsTranslateMixin.js';
 
-import './GrampsjsPasswordManagerPolyfill'
-import {sharedStyles} from '../SharedStyles.js'
-import {apiGetTokens, apiResetPassword, apiRegisterUser} from '../api.js'
-import {fireEvent} from '../util.js'
-import {GrampsjsTranslateMixin} from '../mixins/GrampsjsTranslateMixin.js'
-
-const BASE_DIR = ''
+const BASE_DIR = '';
 
 class GrampsjsLogin extends GrampsjsTranslateMixin(LitElement) {
-  static get styles () {
+  static get styles() {
     return [
       sharedStyles,
       css`
+        #login-container {
+          margin: auto;
+          height: 100%;
+          max-width: 20em;
+        }
 
-      #login-container {
-        margin: auto;
-        height: 100%;
-        max-width: 20em;
-      }
+        #login-form {
+          position: relative;
+          top: 25vh;
+        }
 
-      #login-form {
-        position: relative;
-        top: 25vh;
-      }
+        #login-form mwc-textfield {
+          width: 100%;
+          margin-bottom: 0.7em;
+        }
 
-      #login-form mwc-textfield {
-        width: 100%;
-        margin-bottom: 0.7em;
-      }
+        #login-form mwc-button {
+        }
 
-      #login-form mwc-button {
-      }
+        p.reset-link {
+          padding-top: 1em;
+          font-size: 0.9em;
+        }
 
-      p.reset-link {
-        padding-top: 1em;
-        font-size: 0.9em;
-      }
+        p.success {
+          padding-top: 1em;
+          color: #4caf50;
+          font-size: 1.2em;
+          font-weight: 400;
+          --mdc-icon-size: 1.6em;
+          line-height: 1.4em;
+          text-align: center;
+        }
 
-      p.success {
-        padding-top: 1em;
-        color: #4CAF50;
-        font-size: 1.2em;
-        font-weight: 400;
-        --mdc-icon-size: 1.6em;
-        line-height: 1.4em;
-        text-align: center;
-      }
-
-      mwc-circular-progress {
-        --mdc-theme-primary: white;
-      }
-
-      `
-    ]
+        mwc-circular-progress {
+          --mdc-theme-primary: white;
+        }
+      `,
+    ];
   }
 
-  static get properties () {
+  static get properties() {
     return {
-      resetpw: {type: Boolean},
-      register: {type: Boolean},
-      isFormValid: {type: Boolean},
-      credentials: {type: Object}
-    }
+      resetpw: { type: Boolean },
+      register: { type: Boolean },
+      isFormValid: { type: Boolean },
+      credentials: { type: Object },
+    };
   }
 
-  constructor () {
-    super()
-    this.resetpw = false
-    this.register = false
-    this.isFormValid = false
-    this.credentials = {}
+  constructor() {
+    super();
+    this.resetpw = false;
+    this.register = false;
+    this.isFormValid = false;
+    this.credentials = {};
   }
 
-  render () {
+  render() {
     if (this.resetpw) {
-      return this._renderResetPw()
+      return this._renderResetPw();
     }
     if (this.register) {
-      return this._renderRegister()
+      return this._renderRegister();
     }
-    return this._renderLogin()
+    return this._renderLogin();
   }
 
-  _renderLogin () {
+  _renderLogin() {
     return html`
-    <div id="login-container">
-      <form id="login-form" action="${BASE_DIR}/" @keydown="${this._handleLoginKey}">
-        <mwc-textfield
-          outlined
-          id="username"
-          label="Username"
-          @input="${this._credChanged}"
-          value="${this.credentials.username || ''}"
-        ></mwc-textfield>
-        <mwc-textfield
-          outlined
-          id="password"
-          label="Password"
-          type="password"
-          @input="${this._credChanged}"
-          value="${this.credentials.password || ''}"
-        ></mwc-textfield>
-        <mwc-button raised label="submit" type="submit" @click="${this._submitLogin}">
-          <span slot="trailingIcon" style="display:none;">
-            <mwc-circular-progress indeterminate density="-7" closed id="login-progress">
-            </mwc-circular-progress>
-          </span>
-        </mwc-button>
-        <p class="reset-link">
-          <span class="link" @click="${() => { this.resetpw = true }}"
-          >Lost password?</span>
-        </p>
-        <p class="reset-link">
-          <span class="link" @click="${() => { this.register = true }}"
-          >Register new account</span>
-        </p>
-      </form>
-      <grampsjs-password-manager-polyfill
-        .credentials=${this.credentials}
-        @form-submitted=${this._submitLogin}
-        @value-changed=${this._loginFormChanged}
-      ></grampsjs-password-manager-polyfill>
-    </div>
-    `
+      <div id="login-container">
+        <form
+          id="login-form"
+          action="${BASE_DIR}/"
+          @keydown="${this._handleLoginKey}"
+        >
+          <mwc-textfield
+            outlined
+            id="username"
+            label="${this._('Username')}"
+            @input="${this._credChanged}"
+            value="${this.credentials.username || ''}"
+          ></mwc-textfield>
+          <mwc-textfield
+            outlined
+            id="password"
+            label="${this._('Password')}"
+            type="password"
+            @input="${this._credChanged}"
+            value="${this.credentials.password || ''}"
+          ></mwc-textfield>
+          <mwc-button
+            raised
+            label="${this._('login')}"
+            type="submit"
+            @click="${this._submitLogin}"
+          >
+            <span slot="trailingIcon" style="display:none;">
+              <mwc-circular-progress
+                indeterminate
+                density="-7"
+                closed
+                id="login-progress"
+              >
+              </mwc-circular-progress>
+            </span>
+          </mwc-button>
+          <p class="reset-link">
+            <span
+              class="link"
+              @click="${() => {
+                this.resetpw = true;
+              }}"
+              >${this._('Lost password?')}</span
+            >
+          </p>
+          <p class="reset-link">
+            <span
+              class="link"
+              @click="${() => {
+                this.register = true;
+              }}"
+              >${this._('Register new account')}</span
+            >
+          </p>
+        </form>
+        <grampsjs-password-manager-polyfill
+          .credentials=${this.credentials}
+          @form-submitted=${this._submitLogin}
+          @value-changed=${this._loginFormChanged}
+        ></grampsjs-password-manager-polyfill>
+      </div>
+    `;
   }
 
-  firstUpdated () {
-    const pf = this.shadowRoot.querySelector('grampsjs-password-manager-polyfill')
+  firstUpdated() {
+    const pf = this.shadowRoot.querySelector(
+      'grampsjs-password-manager-polyfill'
+    );
     if (pf !== null) {
-      pf.boundingRect = this.getBoundingClientRect()
+      pf.boundingRect = this.getBoundingClientRect();
     }
   }
 
-  _credChanged (e) {
-    this.credentials = {...this.credentials, [e.target.id]: e.target.value}
+  _credChanged(e) {
+    this.credentials = { ...this.credentials, [e.target.id]: e.target.value };
   }
 
-  _renderResetPw () {
+  _renderResetPw() {
     return html`
-    <div id="login-container">
-      <form id="login-form" action="${BASE_DIR}/">
-        <div id="inner-form">
-          <mwc-textfield outlined id="username" label="Username" type="text"></mwc-textfield>
-          <mwc-button raised label="reset password" type="submit" @click="${this._resetPw}">
-            <span slot="trailingIcon" style="display:none;">
-              <mwc-circular-progress indeterminate density="-7" closed id="login-progress">
-              </mwc-circular-progress>
-            </span>
-          </mwc-button>
-        </div>
-        <p class="success" id="reset-success" style="display:none;">
-          <mwc-icon>check_circle</mwc-icon><br>
-          A password reset link has been sent by e-mail.
-        </p>
-        <p class="reset-link">
-          <span class="link" @click="${() => { this.resetpw = false }}"
-          >Back</span>
-        </p>
-      </form>
-    </div>
-    `
+      <div id="login-container">
+        <form id="login-form" action="${BASE_DIR}/">
+          <div id="inner-form">
+            <mwc-textfield
+              outlined
+              id="username"
+              label="Username"
+              type="text"
+            ></mwc-textfield>
+            <mwc-button
+              raised
+              label="reset password"
+              type="submit"
+              @click="${this._resetPw}"
+            >
+              <span slot="trailingIcon" style="display:none;">
+                <mwc-circular-progress
+                  indeterminate
+                  density="-7"
+                  closed
+                  id="login-progress"
+                >
+                </mwc-circular-progress>
+              </span>
+            </mwc-button>
+          </div>
+          <p class="success" id="reset-success" style="display:none;">
+            <mwc-icon>check_circle</mwc-icon><br />
+            A password reset link has been sent by e-mail.
+          </p>
+          <p class="reset-link">
+            <span
+              class="link"
+              @click="${() => {
+                this.resetpw = false;
+              }}"
+              >Back</span
+            >
+          </p>
+        </form>
+      </div>
+    `;
   }
 
-  _renderRegister () {
+  _renderRegister() {
     return html`
-    <div id="login-container">
-      <form id="login-form" action="${BASE_DIR}/" @keydown="${this._checkFormValid}">
-        <div id="inner-form">
-          <mwc-textfield required outlined id="username" label="Username" type="text"></mwc-textfield>
-          <mwc-textfield required outlined id="password" label="Password" type="password"></mwc-textfield>
-          <mwc-textfield required outlined id="email" label="E-mail" type="email"></mwc-textfield>
-          <mwc-textfield required outlined id="fullname" label="Full name" type="text"></mwc-textfield>
-          <mwc-button raised label="submit" type="submit" @click="${this._register}"
-            ?disabled="${!this.isFormValid}">
-            <span slot="trailingIcon" style="display:none;">
-              <mwc-circular-progress indeterminate density="-7" closed id="login-progress">
-              </mwc-circular-progress>
-            </span>
-          </mwc-button>
-        </div>
-        <p class="success" id="register-success" style="display:none;">
-          <mwc-icon>check_circle</mwc-icon><br>
-          New account registered successfully.
-          <br>Please confirm your e-mail address by
-          clicking the link in the e-mail you received and then wait for the site owner
-          to activate your account.
-        </p>
-        <p class="reset-link">
-          Already have an account? <span class="link" @click="${() => { this.register = false }}"
-          >Login</span>
-        </p>
-      </form>
-    </div>
-    `
+      <div id="login-container">
+        <form
+          id="login-form"
+          action="${BASE_DIR}/"
+          @keydown="${this._checkFormValid}"
+        >
+          <div id="inner-form">
+            <mwc-textfield
+              required
+              outlined
+              id="username"
+              label="Username"
+              type="text"
+            ></mwc-textfield>
+            <mwc-textfield
+              required
+              outlined
+              id="password"
+              label="Password"
+              type="password"
+            ></mwc-textfield>
+            <mwc-textfield
+              required
+              outlined
+              id="email"
+              label="E-mail"
+              type="email"
+            ></mwc-textfield>
+            <mwc-textfield
+              required
+              outlined
+              id="fullname"
+              label="Full name"
+              type="text"
+            ></mwc-textfield>
+            <mwc-button
+              raised
+              label="submit"
+              type="submit"
+              @click="${this._register}"
+              ?disabled="${!this.isFormValid}"
+            >
+              <span slot="trailingIcon" style="display:none;">
+                <mwc-circular-progress
+                  indeterminate
+                  density="-7"
+                  closed
+                  id="login-progress"
+                >
+                </mwc-circular-progress>
+              </span>
+            </mwc-button>
+          </div>
+          <p class="success" id="register-success" style="display:none;">
+            <mwc-icon>check_circle</mwc-icon><br />
+            New account registered successfully.
+            <br />Please confirm your e-mail address by clicking the link in the
+            e-mail you received and then wait for the site owner to activate
+            your account.
+          </p>
+          <p class="reset-link">
+            Already have an account?
+            <span
+              class="link"
+              @click="${() => {
+                this.register = false;
+              }}"
+              >Login</span
+            >
+          </p>
+        </form>
+      </div>
+    `;
   }
 
-  _checkFormValid () {
-    const fields = Array.from(this.shadowRoot.querySelectorAll('mwc-textfield'))
-    this.isFormValid = fields.every(el => el?.validity?.valid)
+  _checkFormValid() {
+    const fields = Array.from(
+      this.shadowRoot.querySelectorAll('mwc-textfield')
+    );
+    this.isFormValid = fields.every(el => el?.validity?.valid);
   }
 
-  _handleLoginKey (event) {
+  _handleLoginKey(event) {
     if (event.code === 'Enter') {
-      this._submitLogin()
+      this._submitLogin();
     }
   }
 
-  async _submitLogin () {
-    const submitProgress = this.shadowRoot.getElementById('login-progress')
-    submitProgress.parentElement.style.display = 'block'
-    submitProgress.closed = false
-    apiGetTokens(this.credentials.username, this.credentials.password)
-      .then((res) => {
+  async _submitLogin() {
+    const submitProgress = this.shadowRoot.getElementById('login-progress');
+    submitProgress.parentElement.style.display = 'block';
+    submitProgress.closed = false;
+    apiGetTokens(this.credentials.username, this.credentials.password).then(
+      res => {
         if ('error' in res) {
-          submitProgress.parentElement.style.display = 'none'
-          submitProgress.closed = true
-          this._showError(res.error)
+          submitProgress.parentElement.style.display = 'none';
+          submitProgress.closed = true;
+          this._showError(res.error);
         } else {
-          document.location.href = '/'
+          document.location.href = '/';
         }
-      })
+      }
+    );
   }
 
-  _loginFormChanged (ev) {
-    this.credentials = {...this.credentials, ...ev.detail.value}
+  _loginFormChanged(ev) {
+    this.credentials = { ...this.credentials, ...ev.detail.value };
   }
 
-  async _resetPw () {
-    const userField = this.shadowRoot.getElementById('username')
+  async _resetPw() {
+    const userField = this.shadowRoot.getElementById('username');
     if (userField.value === '') {
-      this._showError('Username must not be empty.')
-      return
+      this._showError('Username must not be empty.');
+      return;
     }
-    const res = await apiResetPassword(userField.value)
-    const innerForm = this.shadowRoot.getElementById('inner-form')
-    const divSuccess = this.shadowRoot.getElementById('reset-success')
+    const res = await apiResetPassword(userField.value);
+    const innerForm = this.shadowRoot.getElementById('inner-form');
+    const divSuccess = this.shadowRoot.getElementById('reset-success');
     if ('error' in res) {
-      this._showError(res.error)
+      this._showError(res.error);
     } else {
-      divSuccess.style.display = 'block'
-      innerForm.style.display = 'none'
+      divSuccess.style.display = 'block';
+      innerForm.style.display = 'none';
     }
   }
 
-  async _register () {
-    const userField = this.shadowRoot.getElementById('username')
-    const pwField = this.shadowRoot.getElementById('password')
-    const emailField = this.shadowRoot.getElementById('email')
-    const nameField = this.shadowRoot.getElementById('fullname')
-    const res = await apiRegisterUser(userField.value, pwField.value, emailField.value, nameField.value)
-    const innerForm = this.shadowRoot.getElementById('inner-form')
-    const divSuccess = this.shadowRoot.getElementById('register-success')
+  async _register() {
+    const userField = this.shadowRoot.getElementById('username');
+    const pwField = this.shadowRoot.getElementById('password');
+    const emailField = this.shadowRoot.getElementById('email');
+    const nameField = this.shadowRoot.getElementById('fullname');
+    const res = await apiRegisterUser(
+      userField.value,
+      pwField.value,
+      emailField.value,
+      nameField.value
+    );
+    const innerForm = this.shadowRoot.getElementById('inner-form');
+    const divSuccess = this.shadowRoot.getElementById('register-success');
     if ('error' in res) {
-      this._showError(res.error)
+      this._showError(res.error);
     } else {
-      divSuccess.style.display = 'block'
-      innerForm.style.display = 'none'
+      divSuccess.style.display = 'block';
+      innerForm.style.display = 'none';
     }
   }
 
-  _showError (message) {
-    fireEvent(this, 'grampsjs:error', {message})
+  _showError(message) {
+    fireEvent(this, 'grampsjs:error', { message });
   }
 }
 
-window.customElements.define('grampsjs-login', GrampsjsLogin)
+window.customElements.define('grampsjs-login', GrampsjsLogin);
