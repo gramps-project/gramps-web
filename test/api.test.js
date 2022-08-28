fa845cd02c2ff653cb5b8a86bc9b4a509f34b4fb
David Straub <straub@protonmail.com>
2022-08-28 11:16:31 +0200

    Fix lint error in test

--- test/api.test.js
+++ test/api.test.js
@@ -1,32 +1,28 @@
-import fetchMock from 'fetch-mock/esm/client'
+import fetchMock from 'fetch-mock/esm/client';
 import { expect } from '@open-wc/testing';
 
-import { apiGetAuthToken, apiGet } from '../src/api.js'
-
+import { apiGet } from '../src/api.js';
 
 describe('API', () => {
   beforeEach(() => {
-    fetchMock.get('http://myapi:1234/api/my-endpoint', {hello: 'world'})
-    fetchMock.post('http://myapi:1234/api/token/', {token: 'world'})
-  })
+    fetchMock.get('http://myapi:1234/api/my-endpoint', { hello: 'world' });
+    fetchMock.post('http://myapi:1234/api/token/', { token: 'world' });
+  });
 
   afterEach(() => {
-    fetchMock.restore()
-  })
+    fetchMock.restore();
+  });
 
   describe('Test API GET', () => {
     it('API get dummy test', async () => {
-      const data = await apiGet('http://myapi:1234', null, null, '/api/my-endpoint')
-      expect(fetchMock.called()).to.be.true
-      expect(data).to.eql({hello: 'world'})
-    })
-  })
-
-    describe('Test get token', () => {
-      it('Token dummy test', async () => {
-        const data = await apiGetAuthToken('http://myapi:1234', 'user', 'password')
-        expect(fetchMock.called()).to.be.true
-        expect(data).to.eql({token: 'world'})
-      })
-    })
-})
+      const data = await apiGet(
+        'http://myapi:1234',
+        null,
+        null,
+        '/api/my-endpoint'
+      );
+      expect(fetchMock.called()).to.be.true;
+      expect(data).to.eql({ hello: 'world' });
+    });
+  });
+});
