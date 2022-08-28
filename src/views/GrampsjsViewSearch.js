90ac6505dfa37fa9cb6190b5d812129416cb2c8b
David Straub <straub@protonmail.com>
2022-08-28 11:21:10 +0200

    Fix more linter errors

--- src/views/GrampsjsViewSearch.js
+++ src/views/GrampsjsViewSearch.js
@@ -1,290 +1,302 @@
-import {html, css} from 'lit'
+import { html, css } from 'lit';
 
-import {GrampsjsView} from './GrampsjsView.js'
-import '../components/GrampsjsSearchResults.js'
-import '../components/GrampsjsPagination.js'
-import '../components/GrampsjsButtonToggle.js'
-import {apiGet} from '../api.js'
-import {objectTypeToEndpoint, objectIcon, debounce} from '../util.js'
-import '@material/mwc-textfield'
+import { GrampsjsView } from './GrampsjsView.js';
+import '../components/GrampsjsSearchResults.js';
+import '../components/GrampsjsPagination.js';
+import '../components/GrampsjsButtonToggle.js';
+import { apiGet } from '../api.js';
+import { objectTypeToEndpoint, objectIcon, debounce } from '../util.js';
+import '@material/mwc-textfield';
+
+function capitalize(string) {
+  return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+}
 
 export class GrampsjsViewSearch extends GrampsjsView {
-  static get styles () {
+  static get styles() {
     return [
       super.styles,
       css`
-      #search-field-container {
-        text-align: center;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        max-width: 100%;
-        min-width: 80%;
-      }
+        #search-field-container {
+          text-align: center;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          max-width: 100%;
+          min-width: 80%;
+        }
 
-      mwc-textfield#search-field {
-        --mdc-shape-small: 28px;
-        --mdc-typography-subtitle1-font-size: 22px;
-        --mdc-typography-subtitle1-font-weight: 300;
-        --mdc-text-field-idle-line-color:	rgba(0, 0, 0, 0.2);
-        width: calc(100% - 70px);
-        margin: 30px auto;
-      }
+        mwc-textfield#search-field {
+          --mdc-shape-small: 28px;
+          --mdc-typography-subtitle1-font-size: 22px;
+          --mdc-typography-subtitle1-font-weight: 300;
+          --mdc-text-field-idle-line-color: rgba(0, 0, 0, 0.2);
+          width: calc(100% - 70px);
+          margin: 30px auto;
+        }
 
-      #search-field-container mwc-icon-button {
-        color: rgba(0, 0, 0, 0.5);
-        --mdc-icon-size: 26px;
-        --mdc-icon-button-size: 55px;
-        position: relative;
-        top: -2px;
+        #search-field-container mwc-icon-button {
+          color: rgba(0, 0, 0, 0.5);
+          --mdc-icon-size: 26px;
+          --mdc-icon-button-size: 55px;
+          position: relative;
+          top: -2px;
+        }
 
-      }
-
-      grampsjs-button-toggle {
-        margin-right: 0.35em;
-        margin-bottom: 0.35em;
-        display: inline-block;
-      }
-
-    `]
+        grampsjs-button-toggle {
+          margin-right: 0.35em;
+          margin-bottom: 0.35em;
+          display: inline-block;
+        }
+      `,
+    ];
   }
 
-  static get properties () {
+  static get properties() {
     return {
-      _data: {type: Array},
-      _totalCount: {type: Number},
-      _page: {type: Number},
-      _pages: {type: Number},
-      _objectTypes: {type: Object}
-
-    }
+      _data: { type: Array },
+      _totalCount: { type: Number },
+      _page: { type: Number },
+      _pages: { type: Number },
+      _objectTypes: { type: Object },
+    };
   }
 
-  constructor () {
-    super()
-    this._data = []
-    this._totalCount = -1
-    this._page = 1
-    this._pages = -1
+  constructor() {
+    super();
+    this._data = [];
+    this._totalCount = -1;
+    this._page = 1;
+    this._pages = -1;
     this._objectTypes = Object.fromEntries(
-      Object.keys(objectTypeToEndpoint).filter(key => key !== 'tag').map(key => [key, true])
-    )
+      Object.keys(objectTypeToEndpoint)
+        .filter(key => key !== 'tag')
+        .map(key => [key, true])
+    );
   }
 
-  renderContent () {
+  renderContent() {
     return html`
-    <h2>Search</h2>
+      <h2>Search</h2>
 
-    <div id="search-field-container">
-      <mwc-textfield id="search-field" outlined icon="search" @keydown="${this._handleSearchKey}">
-      </mwc-textfield>
-      <mwc-icon-button icon="search" @click="${() => this._executeSearch()}">
-      </mwc-icon-button>
-    </div>
+      <div id="search-field-container">
+        <mwc-textfield
+          id="search-field"
+          outlined
+          icon="search"
+          @keydown="${this._handleSearchKey}"
+        >
+        </mwc-textfield>
+        <mwc-icon-button icon="search" @click="${() => this._executeSearch()}">
+        </mwc-icon-button>
+      </div>
 
-    ${this.renderFilters()}
-    ${(this._totalCount === -1 && !Object.values(this._objectTypes).some(Boolean))
-    ? html`<p>${this._('Select at least one object type')}</p>`
-    : ''}
-      ${this._totalCount === 0
-    ? html`<p>${this._('No items')}</p>`
-    : ''}
-          ${this._totalCount > 0 ? html`<p>Total: ${this._totalCount}</p>` : ''}
-    <grampsjs-search-results
-      .data="${this._data}"
-      .strings="${this.strings}"
-    ></grampsjs-search-results>
+      ${this.renderFilters()}
+      ${this._totalCount === -1 &&
+      !Object.values(this._objectTypes).some(Boolean)
+        ? html`<p>${this._('Select at least one object type')}</p>`
+        : ''}
+      ${this._totalCount === 0 ? html`<p>${this._('No items')}</p>` : ''}
+      ${this._totalCount > 0 ? html`<p>Total: ${this._totalCount}</p>` : ''}
+      <grampsjs-search-results
+        .data="${this._data}"
+        .strings="${this.strings}"
+      ></grampsjs-search-results>
 
-    ${this._totalCount > 0
-    ? html`
-    <grampsjs-pagination
-      page="${this._page}"
-      pages="${this._pages}"
-      .strings="${this.strings}"
-      @page:changed="${this._handlePageChanged}"
-      ></grampsjs-pagination>
-    `
-    : ''}
-    `
+      ${this._totalCount > 0
+        ? html`
+            <grampsjs-pagination
+              page="${this._page}"
+              pages="${this._pages}"
+              .strings="${this.strings}"
+              @page:changed="${this._handlePageChanged}"
+            ></grampsjs-pagination>
+          `
+        : ''}
+    `;
   }
 
-  renderFilters () {
+  renderFilters() {
     return html`
-    <div @grampsjs-button-toggle:toggle="${this._handleFilterToggle}">
-      <grampsjs-button-toggle
-        ?checked="${Object.values(this._objectTypes).every(Boolean)}"
-        icon=""
-        id="toggle-all"
-      >
-        ${this._('All')}
-      </grampsjs-button-toggle>
-      ${Object.keys(this._objectTypes).map(key => html`<grampsjs-button-toggle
-        ?checked="${this._objectTypes[key]}"
-        icon="${objectIcon[key]}"
-        id="toggle-${key}"
-      >
-        ${this._(capitalize(objectTypeToEndpoint[key]))}
-      </grampsjs-button-toggle>`
-  )}
-    </div>
-  `
+      <div @grampsjs-button-toggle:toggle="${this._handleFilterToggle}">
+        <grampsjs-button-toggle
+          ?checked="${Object.values(this._objectTypes).every(Boolean)}"
+          icon=""
+          id="toggle-all"
+        >
+          ${this._('All')}
+        </grampsjs-button-toggle>
+        ${Object.keys(this._objectTypes).map(
+          key => html`<grampsjs-button-toggle
+            ?checked="${this._objectTypes[key]}"
+            icon="${objectIcon[key]}"
+            id="toggle-${key}"
+          >
+            ${this._(capitalize(objectTypeToEndpoint[key]))}
+          </grampsjs-button-toggle>`
+        )}
+      </div>
+    `;
   }
 
-  _handleFilterToggle (e) {
-    const key = e.target.id.split('-', 2)[1]
+  _handleFilterToggle(e) {
+    const key = e.target.id.split('-', 2)[1];
     if (key === 'all') {
       this._objectTypes = Object.fromEntries(
-        Object.keys(this._objectTypes).map(key => [key, e.detail.checked])
-      )
+        Object.keys(this._objectTypes).map(key_ => [key_, e.detail.checked])
+      );
     } else {
-      this._objectTypes = {...this._objectTypes, [key]: e.detail.checked}
+      this._objectTypes = { ...this._objectTypes, [key]: e.detail.checked };
     }
-    this._page = 1
-    debounce(() => this._executeSearch(), 500)()
+    this._page = 1;
+    debounce(() => this._executeSearch(), 500)();
   }
 
-  _handlePageChanged (event) {
-    this._page = event.detail.page
+  _handlePageChanged(event) {
+    this._page = event.detail.page;
   }
 
-  firstUpdated () {
-    this._focus()
+  firstUpdated() {
+    this._focus();
   }
 
-  _focus (retry = true) {
+  _focus(retry = true) {
     if (this.active) {
-      const el = this.shadowRoot.getElementById('search-field')
+      const el = this.shadowRoot.getElementById('search-field');
       try {
-        el.focus()
+        el.focus();
       } catch (e) {
         // retry once
-        window.setTimeout(() => this._focus(false), 100)
+        if (retry) {
+          window.setTimeout(() => this._focus(false), 100);
+        }
       }
     }
   }
 
-  _unfocus () {
+  _unfocus() {
     if (this.active) {
-      const el = this.shadowRoot.getElementById('search-field')
+      const el = this.shadowRoot.getElementById('search-field');
       try {
-        el.blur()
+        el.blur();
       } catch (e) {
         // retry once
-        window.setTimeout(() => this._blur(false), 100)
+        window.setTimeout(() => this._blur(false), 100);
       }
     }
   }
 
-  _clearBox () {
-    this.shadowRoot.getElementById('search-field').value = ''
+  _clearBox() {
+    this.shadowRoot.getElementById('search-field').value = '';
   }
 
-  _clearAll () {
-    this._clearBox()
-    this._data = []
-    this._totalCount = -1
+  _clearAll() {
+    this._clearBox();
+    this._data = [];
+    this._totalCount = -1;
   }
 
-  _clearPage () {
-    this._data = []
+  _clearPage() {
+    this._data = [];
   }
 
-  update (changed) {
-    super.update(changed)
+  update(changed) {
+    super.update(changed);
     if (changed.has('active')) {
-      this._focus()
+      this._focus();
     }
     if (changed.has('_page') && this._totalCount > 0) {
-      this.loading = true
-      this._executeSearch(this._page)
+      this.loading = true;
+      this._executeSearch(this._page);
     }
   }
 
-  _handleSearchKey (event) {
+  _handleSearchKey(event) {
     if (event.code === 'Enter') {
-      this._executeSearch()
+      this._executeSearch();
     }
     if (event.code === 'Escape') {
-      const query = this.shadowRoot.getElementById('search-field').value
+      const query = this.shadowRoot.getElementById('search-field').value;
       if (query === '') {
-        this._unfocus()
+        this._unfocus();
       } else {
-        this._clearBox()
+        this._clearBox();
       }
     }
   }
 
-  _executeSearch (page = 1) {
-    let query = this.shadowRoot.getElementById('search-field').value
+  _executeSearch(page = 1) {
+    let query = this.shadowRoot.getElementById('search-field').value;
     if (query === '') {
-      this._clearAll()
-      return
+      this._clearAll();
+      return;
     }
     // apply object type filter if necessary
     if (!Object.values(this._objectTypes).every(Boolean)) {
       if (!Object.values(this._objectTypes).some(Boolean)) {
         // all deselected - do nothing
-        this._data = []
-        this._totalCount = -1
-        return
+        this._data = [];
+        this._totalCount = -1;
+        return;
       }
-        query = this._filterQueryByObjectType(query)
-
+      query = this._filterQueryByObjectType(query);
     }
-    this.loading = true
-    this._fetchData(query, page)
+    this.loading = true;
+    this._fetchData(query, page);
   }
 
-  _filterQueryByObjectType (query) {
-    const objectTypes = Object.keys(this._objectTypes).filter(key => this._objectTypes[key])
-    return `${query} (${objectTypes.map(key => `type:${key}`).join(' OR ')})`
+  _filterQueryByObjectType(query) {
+    const objectTypes = Object.keys(this._objectTypes).filter(
+      key => this._objectTypes[key]
+    );
+    return `${query} (${objectTypes.map(key => `type:${key}`).join(' OR ')})`;
   }
 
-  _pageFirst () {
-    this._page = 1
+  _pageFirst() {
+    this._page = 1;
   }
 
-  _pagePrev () {
-    this._page -= 1
+  _pagePrev() {
+    this._page -= 1;
   }
 
-  _pageNext () {
-    this._page += 1
+  _pageNext() {
+    this._page += 1;
   }
 
-  _pageLast () {
-    this._page = this._pages
+  _pageLast() {
+    this._page = this._pages;
   }
 
-  async _fetchData (query, page) {
-    const data = await apiGet(`/api/search/?query=${query}&locale=${this.strings?.__lang__ || 'en'}&profile=all&page=${page}&pagesize=20`)
-    this.loading = false
+  async _fetchData(query, page) {
+    const data = await apiGet(
+      `/api/search/?query=${query}&locale=${
+        this.strings?.__lang__ || 'en'
+      }&profile=all&page=${page}&pagesize=20`
+    );
+    this.loading = false;
     if ('data' in data) {
-      this.error = false
-      this._data = data.data
-      this._totalCount = parseInt(data.total_count)
-      this._pages = Math.ceil(this._totalCount / 20)
+      this.error = false;
+      this._data = data.data;
+      this._totalCount = parseInt(data.total_count, 10);
+      this._pages = Math.ceil(this._totalCount / 20);
     } else if ('error' in data) {
-      this.error = true
-      this._errorMessage = data.error
+      this.error = true;
+      this._errorMessage = data.error;
     }
   }
 
-  connectedCallback () {
-    super.connectedCallback()
-    window.addEventListener('nav', (event) => this._handleNav(event))
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener('nav', event => this._handleNav(event));
   }
 
-  _handleNav (event) {
+  _handleNav(event) {
     if (event.detail.path !== 'search') {
-      return
+      return;
     }
-    this._focus()
+    this._focus();
   }
 }
 
-function capitalize (string) {
-  return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
-}
-
-window.customElements.define('grampsjs-view-search', GrampsjsViewSearch)
+window.customElements.define('grampsjs-view-search', GrampsjsViewSearch);
--- src/views/GrampsjsViewSettingsOnboarding.js
+++ src/views/GrampsjsViewSettingsOnboarding.js
@@ -1,216 +1,247 @@
-import {html, css} from 'lit'
+import { html, css } from 'lit';
 
-import {GrampsjsView} from './GrampsjsView.js'
-import {apiGet, getSettings, updateSettings} from '../api.js'
-import {debounce} from '../util.js'
-import '@material/mwc-button'
-import '@material/mwc-menu'
-import '../components/GrampsjsFormSelectObjectList.js'
+import { GrampsjsView } from './GrampsjsView.js';
+import { apiGet, getSettings, updateSettings } from '../api.js';
+import '@material/mwc-button';
+import '@material/mwc-menu';
+import '../components/GrampsjsFormSelectObjectList.js';
 
 export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
-  static get styles () {
+  static get styles() {
     return [
       super.styles,
       css`
-      .red {
-        --mdc-button-outline-color: #C62828;
-        --mdc-theme-primary: #C62828;
-      }
+        .red {
+          --mdc-button-outline-color: #c62828;
+          --mdc-theme-primary: #c62828;
+        }
 
-      mwc-select {
-        width: 100%;
-        max-width: 30em;
-        margin-bottom: 10px;
-      }
+        mwc-select {
+          width: 100%;
+          max-width: 30em;
+          margin-bottom: 10px;
+        }
 
-      #complete-button {
-        margin-top: 25px;
-      }
-    `]
+        #complete-button {
+          margin-top: 25px;
+        }
+      `,
+    ];
   }
 
-  static get properties () {
+  static get properties() {
     return {
-      homePersonDetails: {type: Object},
-      requireHomePerson: {type: Boolean},
-      _translations: {type: Array},
-      _settings: {type: Object},
-      _langLoading: {type: Boolean},
-      _peopleLoading: {type: Boolean}
-    }
+      homePersonDetails: { type: Object },
+      requireHomePerson: { type: Boolean },
+      _translations: { type: Array },
+      _settings: { type: Object },
+      _langLoading: { type: Boolean },
+      _peopleLoading: { type: Boolean },
+    };
   }
 
-  constructor () {
-    super()
-    this.homePersonDetails = {}
-    this.requireHomePerson = false
-    this._translations = []
-    this._settings = getSettings()
-    this._langLoading = false
-    this._peopleLoading = false
+  constructor() {
+    super();
+    this.homePersonDetails = {};
+    this.requireHomePerson = false;
+    this._translations = [];
+    this._settings = getSettings();
+    this._langLoading = false;
+    this._peopleLoading = false;
   }
 
-  renderContent () {
+  renderContent() {
     return html`
+      <h3>${this._('Select language')}</h3>
 
-    <h3>${this._('Select language')}</h3>
+      ${this.renderLangSelect()}
 
-    ${this.renderLangSelect()}
+      <h3>${this._('Set _Home Person')}</h3>
 
-    <h3>${this._('Set _Home Person')}</h3>
+      ${this.renderPersonSelect()}
 
-    ${this.renderPersonSelect()}
-
-    <mwc-button
-      raised
-      id="complete-button"
-      label="${this._('Complete')}"
-      icon="check_circle"
-      ?disabled="${!this._isCompleted()}"
-      @click=${() => this._doSubmit()}
-    ></mwc-button>
-
-    `
+      <mwc-button
+        raised
+        id="complete-button"
+        label="${this._('Complete')}"
+        icon="check_circle"
+        ?disabled="${!this._isCompleted()}"
+        @click=${() => this._doSubmit()}
+      ></mwc-button>
+    `;
   }
 
-  _isCompleted () {
-    if ((!this.requireHomePerson || this._settings.homePerson) && this._settings.lang) {
-      return true
+  _isCompleted() {
+    if (
+      (!this.requireHomePerson || this._settings.homePerson) &&
+      this._settings.lang
+    ) {
+      return true;
     }
-    return false
+    return false;
   }
 
   // eslint-disable-next-line class-methods-use-this
-  _doSubmit () {
-    this.dispatchEvent(new CustomEvent('onboarding:completed', {bubbles: true, composed: true}))
+  _doSubmit() {
+    this.dispatchEvent(
+      new CustomEvent('onboarding:completed', { bubbles: true, composed: true })
+    );
   }
 
-  renderPersonSelect () {
+  renderPersonSelect() {
     return html`
-    <p>
-      <grampsjs-form-object-list
-        .strings="${this.strings}"
-        .objects=${this.homePersonDetails.handle ? [this.homePersonDetails] : []}
-        id="homeperson-list"
-      ></grampsjs-form-object-list>
-    </p>
-    <p>
-      <grampsjs-form-select-object
-        @select-object:changed="${this._handleHomePerson}"
-        objectType="person"
-        .strings="${this.strings}"
-        id="homeperson-select"
-        label="${this._('Select')}"
-        ?disabled="${this._peopleLoading}"
-      ></grampsjs-form-select-object>
-    </p>
-  `
+      <p>
+        <grampsjs-form-object-list
+          .strings="${this.strings}"
+          .objects=${this.homePersonDetails.handle
+            ? [this.homePersonDetails]
+            : []}
+          id="homeperson-list"
+        ></grampsjs-form-object-list>
+      </p>
+      <p>
+        <grampsjs-form-select-object
+          @select-object:changed="${this._handleHomePerson}"
+          objectType="person"
+          .strings="${this.strings}"
+          id="homeperson-select"
+          label="${this._('Select')}"
+          ?disabled="${this._peopleLoading}"
+        ></grampsjs-form-select-object>
+      </p>
+    `;
   }
 
-  _handleHomePerson (e) {
-    const obj = e.detail.objects[0]
+  _handleHomePerson(e) {
+    const obj = e.detail.objects[0];
     if (obj.object?.gramps_id) {
-      updateSettings({homePerson: obj.object.gramps_id})
-      this._handleStorage()
-      this.homePersonDetails = obj
+      updateSettings({ homePerson: obj.object.gramps_id });
+      this._handleStorage();
+      this.homePersonDetails = obj;
     }
-    e.preventDefault()
-    e.stopPropagation()
+    e.preventDefault();
+    e.stopPropagation();
   }
 
-  renderLangSelect () {
+  renderLangSelect() {
     return html`
-    <mwc-select
-      id="select-language"
-      label="${this._langLoading ? this._('Loading items...') : this._('Language')}"
-      @selected="${this._handleLangSelected}"
-      ?disabled="${this._langLoading}">
-    ${this._translations.map(langObj => html`
-      <mwc-list-item
-        value="${langObj.language}"
-        ?selected="${langObj.language === this._settings.lang}"
-        >${langObj.native}</mwc-list-item>
-      `, this)}
-    </mwc-select>
-  `
+      <mwc-select
+        id="select-language"
+        label="${this._langLoading
+          ? this._('Loading items...')
+          : this._('Language')}"
+        @selected="${this._handleLangSelected}"
+        ?disabled="${this._langLoading}"
+      >
+        ${this._translations.map(
+          langObj => html`
+            <mwc-list-item
+              value="${langObj.language}"
+              ?selected="${langObj.language === this._settings.lang}"
+              >${langObj.native}</mwc-list-item
+            >
+          `,
+          this
+        )}
+      </mwc-select>
+    `;
   }
 
-  _handleLangSelected (event) {
-    const i = event.detail.index
+  _handleLangSelected(event) {
+    const i = event.detail.index;
     if (i !== null && i !== undefined && i < this._translations.length) {
-      const key = this._translations[i].language
-      updateSettings({lang: key})
-      this._handleStorage()
+      const key = this._translations[i].language;
+      updateSettings({ lang: key });
+      this._handleStorage();
     }
   }
 
-  connectedCallback () {
-    super.connectedCallback()
-    window.addEventListener('storage', this._handleStorage.bind(this))
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener('storage', this._handleStorage.bind(this));
   }
 
-  _handleStorage () {
-    this._settings = getSettings()
-    window.dispatchEvent(new CustomEvent('settings:changed', {bubbles: true, composed: true}))
+  _handleStorage() {
+    this._settings = getSettings();
+    window.dispatchEvent(
+      new CustomEvent('settings:changed', { bubbles: true, composed: true })
+    );
   }
 
-  firstUpdated () {
+  firstUpdated() {
     if (this.active) {
-      this._fetchDataLang()
-      this._fetchDataHomePerson()
+      this._fetchDataLang();
+      this._fetchDataHomePerson();
     }
   }
 
-  update (changed) {
-    super.update(changed)
+  update(changed) {
+    super.update(changed);
     if (changed.has('active') && this.active) {
       if (!this._langLoading && this._translations.length === 0) {
-        this._fetchDataLang()
+        this._fetchDataLang();
       }
-      if (!this._peopleLoading && (this.homePersonDetails?.object?.grampd_id !== this._settings.homePerson)) {
-        this._fetchDataHomePerson()
+      if (
+        !this._peopleLoading &&
+        this.homePersonDetails?.object?.grampd_id !== this._settings.homePerson
+      ) {
+        this._fetchDataHomePerson();
       }
     }
   }
 
-  async _fetchDataLang () {
-    this.loading = true
-    this._langLoading = true
-    const dataTrans = await apiGet('/api/translations/')
+  async _fetchDataLang() {
+    this.loading = true;
+    this._langLoading = true;
+    const dataTrans = await apiGet('/api/translations/');
     if ('data' in dataTrans) {
-      this.error = false
-      this._translations = dataTrans.data
+      this.error = false;
+      this._translations = dataTrans.data;
     } else if ('error' in dataTrans) {
-      this.error = true
-      this._errorMessage = dataTrans.error
-      return
+      this.error = true;
+      this._errorMessage = dataTrans.error;
+      return;
     }
-    this._langLoading = false
+    this._langLoading = false;
     if (!this._peopleLoading) {
-      this.loading = false
+      this.loading = false;
     }
   }
 
-  async _fetchDataHomePerson () {
-    if (this._settings.homePerson && (this.homePersonDetails?.object?.grampd_id !== this._settings.homePerson)) {
-      this.loading = true
-      this._peopleLoading = true
-      const seachPhrase = `${this._settings.homePerson}`
-      const dataPeople = await apiGet(`/api/search/?locale=${this.strings?.__lang__ || 'en'}&profile=self&query=${encodeURIComponent(`${seachPhrase} AND type:person`)}`)
+  async _fetchDataHomePerson() {
+    if (
+      this._settings.homePerson &&
+      this.homePersonDetails?.object?.grampd_id !== this._settings.homePerson
+    ) {
+      this.loading = true;
+      this._peopleLoading = true;
+      const seachPhrase = `${this._settings.homePerson}`;
+      const dataPeople = await apiGet(
+        `/api/search/?locale=${
+          this.strings?.__lang__ || 'en'
+        }&profile=self&query=${encodeURIComponent(
+          `${seachPhrase} AND type:person`
+        )}`
+      );
       if ('data' in dataPeople) {
         this.error = false;
-        [this.homePersonDetails] = dataPeople.data.filter(obj => obj.object?.gramps_id === this._settings.homePerson)
+        [this.homePersonDetails] = dataPeople.data.filter(
+          obj => obj.object?.gramps_id === this._settings.homePerson
+        );
       } else if ('error' in dataPeople) {
-        this.error = true
-        this._errorMessage = dataPeople.error
-        return
+        this.error = true;
+        this._errorMessage = dataPeople.error;
+        return;
       }
-      this._peopleLoading = false
+      this._peopleLoading = false;
       if (!this._langLoading) {
-        this.loading = false
+        this.loading = false;
       }
     }
   }
 }
 
-window.customElements.define('grampsjs-view-settings-onboarding', GrampsjsViewSettingsOnboarding)
+window.customElements.define(
+  'grampsjs-view-settings-onboarding',
+  GrampsjsViewSettingsOnboarding
+);
