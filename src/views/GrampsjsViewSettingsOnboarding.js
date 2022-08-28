1b486a270f88e107725fbc2397c0366255ab6db3
David Straub <straub@protonmail.com>
2022-08-28 11:40:05 +0200

    Remove semi

--- src/views/GrampsjsViewSettingsOnboarding.js
+++ src/views/GrampsjsViewSettingsOnboarding.js
@@ -1,10 +1,10 @@
-import { html, css } from 'lit';
+import {html, css} from 'lit'
 
-import { GrampsjsView } from './GrampsjsView.js';
-import { apiGet, getSettings, updateSettings } from '../api.js';
-import '@material/mwc-button';
-import '@material/mwc-menu';
-import '../components/GrampsjsFormSelectObjectList.js';
+import {GrampsjsView} from './GrampsjsView.js'
+import {apiGet, getSettings, updateSettings} from '../api.js'
+import '@material/mwc-button'
+import '@material/mwc-menu'
+import '../components/GrampsjsFormSelectObjectList.js'
 
 export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
   static get styles() {
@@ -26,28 +26,28 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
           margin-top: 25px;
         }
       `,
-    ];
+    ]
   }
 
   static get properties() {
     return {
-      homePersonDetails: { type: Object },
-      requireHomePerson: { type: Boolean },
-      _translations: { type: Array },
-      _settings: { type: Object },
-      _langLoading: { type: Boolean },
-      _peopleLoading: { type: Boolean },
-    };
+      homePersonDetails: {type: Object},
+      requireHomePerson: {type: Boolean},
+      _translations: {type: Array},
+      _settings: {type: Object},
+      _langLoading: {type: Boolean},
+      _peopleLoading: {type: Boolean},
+    }
   }
 
   constructor() {
-    super();
-    this.homePersonDetails = {};
-    this.requireHomePerson = false;
-    this._translations = [];
-    this._settings = getSettings();
-    this._langLoading = false;
-    this._peopleLoading = false;
+    super()
+    this.homePersonDetails = {}
+    this.requireHomePerson = false
+    this._translations = []
+    this._settings = getSettings()
+    this._langLoading = false
+    this._peopleLoading = false
   }
 
   renderContent() {
@@ -68,7 +68,7 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
         ?disabled="${!this._isCompleted()}"
         @click=${() => this._doSubmit()}
       ></mwc-button>
-    `;
+    `
   }
 
   _isCompleted() {
@@ -76,16 +76,16 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
       (!this.requireHomePerson || this._settings.homePerson) &&
       this._settings.lang
     ) {
-      return true;
+      return true
     }
-    return false;
+    return false
   }
 
   // eslint-disable-next-line class-methods-use-this
   _doSubmit() {
     this.dispatchEvent(
-      new CustomEvent('onboarding:completed', { bubbles: true, composed: true })
-    );
+      new CustomEvent('onboarding:completed', {bubbles: true, composed: true})
+    )
   }
 
   renderPersonSelect() {
@@ -109,18 +109,18 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
           ?disabled="${this._peopleLoading}"
         ></grampsjs-form-select-object>
       </p>
-    `;
+    `
   }
 
   _handleHomePerson(e) {
-    const obj = e.detail.objects[0];
+    const obj = e.detail.objects[0]
     if (obj.object?.gramps_id) {
-      updateSettings({ homePerson: obj.object.gramps_id });
-      this._handleStorage();
-      this.homePersonDetails = obj;
+      updateSettings({homePerson: obj.object.gramps_id})
+      this._handleStorage()
+      this.homePersonDetails = obj
     }
-    e.preventDefault();
-    e.stopPropagation();
+    e.preventDefault()
+    e.stopPropagation()
   }
 
   renderLangSelect() {
@@ -144,67 +144,67 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
           this
         )}
       </mwc-select>
-    `;
+    `
   }
 
   _handleLangSelected(event) {
-    const i = event.detail.index;
+    const i = event.detail.index
     if (i !== null && i !== undefined && i < this._translations.length) {
-      const key = this._translations[i].language;
-      updateSettings({ lang: key });
-      this._handleStorage();
+      const key = this._translations[i].language
+      updateSettings({lang: key})
+      this._handleStorage()
     }
   }
 
   connectedCallback() {
-    super.connectedCallback();
-    window.addEventListener('storage', this._handleStorage.bind(this));
+    super.connectedCallback()
+    window.addEventListener('storage', this._handleStorage.bind(this))
   }
 
   _handleStorage() {
-    this._settings = getSettings();
+    this._settings = getSettings()
     window.dispatchEvent(
-      new CustomEvent('settings:changed', { bubbles: true, composed: true })
-    );
+      new CustomEvent('settings:changed', {bubbles: true, composed: true})
+    )
   }
 
   firstUpdated() {
     if (this.active) {
-      this._fetchDataLang();
-      this._fetchDataHomePerson();
+      this._fetchDataLang()
+      this._fetchDataHomePerson()
     }
   }
 
   update(changed) {
-    super.update(changed);
+    super.update(changed)
     if (changed.has('active') && this.active) {
       if (!this._langLoading && this._translations.length === 0) {
-        this._fetchDataLang();
+        this._fetchDataLang()
       }
       if (
         !this._peopleLoading &&
         this.homePersonDetails?.object?.grampd_id !== this._settings.homePerson
       ) {
-        this._fetchDataHomePerson();
+        this._fetchDataHomePerson()
       }
     }
   }
 
   async _fetchDataLang() {
-    this.loading = true;
-    this._langLoading = true;
-    const dataTrans = await apiGet('/api/translations/');
+    this.loading = true
+    this._langLoading = true
+    const dataTrans = await apiGet('/api/translations/')
     if ('data' in dataTrans) {
-      this.error = false;
-      this._translations = dataTrans.data;
+      this.error = false
+      this._translations = dataTrans.data
     } else if ('error' in dataTrans) {
-      this.error = true;
-      this._errorMessage = dataTrans.error;
-      return;
+      this.error = true
+      this._errorMessage = dataTrans.error
+      return
     }
-    this._langLoading = false;
+    this._langLoading = false
     if (!this._peopleLoading) {
-      this.loading = false;
+      this.loading = false
     }
   }
 
@@ -213,29 +213,29 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
       this._settings.homePerson &&
       this.homePersonDetails?.object?.grampd_id !== this._settings.homePerson
     ) {
-      this.loading = true;
-      this._peopleLoading = true;
-      const seachPhrase = `${this._settings.homePerson}`;
+      this.loading = true
+      this._peopleLoading = true
+      const seachPhrase = `${this._settings.homePerson}`
       const dataPeople = await apiGet(
         `/api/search/?locale=${
           this.strings?.__lang__ || 'en'
         }&profile=self&query=${encodeURIComponent(
           `${seachPhrase} AND type:person`
         )}`
-      );
+      )
       if ('data' in dataPeople) {
-        this.error = false;
-        [this.homePersonDetails] = dataPeople.data.filter(
+        this.error = false
+        ;[this.homePersonDetails] = dataPeople.data.filter(
           obj => obj.object?.gramps_id === this._settings.homePerson
-        );
+        )
       } else if ('error' in dataPeople) {
-        this.error = true;
-        this._errorMessage = dataPeople.error;
-        return;
+        this.error = true
+        this._errorMessage = dataPeople.error
+        return
       }
-      this._peopleLoading = false;
+      this._peopleLoading = false
       if (!this._langLoading) {
-        this.loading = false;
+        this.loading = false
       }
     }
   }
@@ -244,4 +244,4 @@ export class GrampsjsViewSettingsOnboarding extends GrampsjsView {
 window.customElements.define(
   'grampsjs-view-settings-onboarding',
   GrampsjsViewSettingsOnboarding
-);
+)
