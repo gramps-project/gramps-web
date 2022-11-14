b7a65385a23b53376c111bd257300d0e13ed4568
David Straub <straub@protonmail.com>
2022-11-14 19:47:37 +0100

    Change style of addresses

--- src/components/GrampsjsAddresses.js
+++ src/components/GrampsjsAddresses.js
@@ -1,36 +1,112 @@
-import {html} from 'lit'
-import {GrampsjsTableBase} from './GrampsjsTableBase.js'
+import {html, LitElement, css} from 'lit'
+import {GrampsjsTranslateMixin} from '../mixins/GrampsjsTranslateMixin.js'
+import {sharedStyles} from '../SharedStyles.js'
 
-export class GrampsjsAddresses extends GrampsjsTableBase {
+export class GrampsjsAddresses extends GrampsjsTranslateMixin(LitElement) {
+  static get styles() {
+    return [
+      sharedStyles,
+      css`
+        dl {
+          clear: left;
+        }
+
+        dl > div {
+          float: none;
+        }
+
+        dl > div > dt {
+          display: inline-block;
+          width: 10em;
+          margin-right: 1em;
+          text-align: right;
+        }
+
+        dl > div > dd {
+          display: inline-block;
+        }
+      `,
+    ]
+  }
+
+  static get properties() {
+    return {
+      data: {type: Object},
+    }
+  }
+
+  constructor() {
+    super()
+    this.data = {}
+  }
+
   render() {
     if (Object.keys(this.data).length === 0) {
       return ''
     }
     return html`
-      <table>
-        <tr>
-          <th>${this._('Date')}</th>
-          <th>${this._('Street')}</th>
-          <th>${this._('Locality')}</th>
-          <th>${this._('City')}</th>
-          <th>${this._('County')}</th>
-          <th>${this._('State')}</th>
-          <th>${this._('Country')}</th>
-        </tr>
-        ${this.data.map(
-          obj => html`
-      <tr>
-        <td>${this._toDate(obj?.date?.dateval)}</td>
-        <td>${obj.street}</td>
-        <td>${obj.locality}</td>
-        <td>${obj.city}</td>
-        <td>${obj.county}</td>
-        <td>${obj.state}</td>
-        <td>${obj.country}</td>
-      </tr>
-    </table>
-    `
-        )}
+    ${this.data.map(
+      obj => html`
+        <dl>
+          ${obj?.date?.dateval
+            ? html`
+                <div>
+                  <dt>${this._('Date')}</dt>
+                  <dd>${this._toDate(obj?.date?.dateval)}</dd>
+                </div>
+              `
+            : ''}
+          ${obj.street
+            ? html`
+                <div>
+                  <dt>${this._('Street')}</dt>
+                  <dd>${obj.street}</dd>
+                </div>
+              `
+            : ''}
+          ${obj.locality
+            ? html`
+                <div>
+                  <dt>${this._('Locality')}</dt>
+                  <dd>${obj.locality}</dd>
+                </div>
+              `
+            : ''}
+          ${obj.city
+            ? html`
+                <div>
+                  <dt>${this._('City')}</dt>
+                  <dd>${obj.city}</dd>
+                </div>
+              `
+            : ''}
+          ${obj.county
+            ? html`
+                <div>
+                  <dt>${this._('County')}</dt>
+                  <dd>${obj.county}</dd>
+                </div>
+              `
+            : ''}
+          ${obj.state
+            ? html`
+                <div>
+                  <dt>${this._('State')}</dt>
+                  <dd>${obj.state}</dd>
+                </div>
+              `
+            : ''}
+          ${obj.country
+            ? html`
+                <div>
+                  <dt>${this._('Country')}</dt>
+                  <dd>${obj.country}</dd>
+                </div>
+              `
+            : ''}
+        </dl>
+      `
+    )}
       </table>
     `
   }
