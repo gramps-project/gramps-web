3ee6c3ae18222e4daea30f5d65a4f45c06a3714b
David Straub <straub@protonmail.com>
2022-11-18 21:51:11 +0100

    Add missing module

---
+++ src/components/GrampsjsFormEditPlaceName.js
@@ -0,0 +1,29 @@
+/*
+Form for adding a new event reference
+*/
+
+import {html} from 'lit'
+
+import './GrampsjsFormString.js'
+import {GrampsjsObjectForm} from './GrampsjsObjectForm.js'
+
+class GrampsjsFormEditPlaceName extends GrampsjsObjectForm {
+  renderForm() {
+    return html`
+      <pre>${JSON.stringify(this.data, null, 2)}</pre>
+      <grampsjs-form-string
+        @formdata:changed="${this._handleFormData}"
+        fullwidth
+        id="place-name-value"
+        label="${this._('Name')}"
+        value="${this.data.value || ''}"
+      >
+      </grampsjs-form-string>
+    `
+  }
+}
+
+window.customElements.define(
+  'grampsjs-form-edit-placename',
+  GrampsjsFormEditPlaceName
+)
