837ceb0548f717180ed984f98ecf5aedce90b13c
David Straub <straub@protonmail.com>
2023-03-20 19:53:45 +0100

    Add missing translations (#191)

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,20 +1,94 @@
 /* eslint-disable camelcase */
-import en from './en.json' assert {type: 'json'}
-import ru from './ru.json' assert {type: 'json'}
+import ar from './ar.json' assert {type: 'json'}
+import bg from './bg.json' assert {type: 'json'}
+import br from './br.json' assert {type: 'json'}
+import ca from './ca.json' assert {type: 'json'}
+import cs from './cs.json' assert {type: 'json'}
+import da from './da.json' assert {type: 'json'}
+import de_AT from './de_AT.json' assert {type: 'json'}
 import de from './de.json' assert {type: 'json'}
+import el from './el.json' assert {type: 'json'}
 import en_GB from './en_GB.json' assert {type: 'json'}
-import pt_PT from './pt_PT.json' assert {type: 'json'}
+import en from './en.json' assert {type: 'json'}
+import eo from './eo.json' assert {type: 'json'}
+import es from './es.json' assert {type: 'json'}
+import fi from './fi.json' assert {type: 'json'}
+import fr from './fr.json' assert {type: 'json'}
+import ga from './ga.json' assert {type: 'json'}
+import he from './he.json' assert {type: 'json'}
+import hr from './hr.json' assert {type: 'json'}
+import hu from './hu.json' assert {type: 'json'}
+import is from './is.json' assert {type: 'json'}
+import it from './it.json' assert {type: 'json'}
+import ja from './ja.json' assert {type: 'json'}
+import lt from './lt.json' assert {type: 'json'}
+import mk from './mk.json' assert {type: 'json'}
 import nb from './nb.json' assert {type: 'json'}
+import nl from './nl.json' assert {type: 'json'}
+import nn from './nn.json' assert {type: 'json'}
+import pl from './pl.json' assert {type: 'json'}
+import pt_BR from './pt_BR.json' assert {type: 'json'}
+import pt_PT from './pt_PT.json' assert {type: 'json'}
+import ro from './ro.json' assert {type: 'json'}
+import ru from './ru.json' assert {type: 'json'}
+import sk from './sk.json' assert {type: 'json'}
+import sl from './sl.json' assert {type: 'json'}
+import sq from './sq.json' assert {type: 'json'}
+import sr from './sr.json' assert {type: 'json'}
 import sv from './sv.json' assert {type: 'json'}
+import ta from './ta.json' assert {type: 'json'}
+import tr from './tr.json' assert {type: 'json'}
+import uk from './uk.json' assert {type: 'json'}
+import vi from './vi.json' assert {type: 'json'}
+import zh_CN from './zh_CN.json' assert {type: 'json'}
+import zh_HK from './zh_HK.json' assert {type: 'json'}
+import zh_TW from './zh_TW.json' assert {type: 'json'}
 
 const languages = {
-  en,
-  ru,
-  de,
+  ar,
+  ca,
+  de_AT,
   en_GB,
+  es,
+  ga,
+  hu,
+  it,
+  mk,
+  nn,
   pt_PT,
+  sk,
+  sr,
+  tr,
+  zh_CN,
+  bg,
+  cs,
+  de,
+  en,
+  fi,
+  he,
+  ja,
   nb,
+  pl,
+  ro,
+  sl,
   sv,
+  uk,
+  zh_HK,
+  br,
+  da,
+  el,
+  eo,
+  fr,
+  hr,
+  is,
+  lt,
+  nl,
+  pt_BR,
+  ru,
+  sq,
+  ta,
+  vi,
+  zh_TW,
 }
 
 const translations = languages
