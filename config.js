65e8ad251a9c079c748196fb8cdf59db9b23be21
David Straub <straub@protonmail.com>
2023-05-08 17:17:03 +0200

    Add frontend config file

---
+++ config.js
@@ -0,0 +1,6 @@
+window.grampsjsConfig = {
+  leafletTileUrl:
+    'https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}{r}.png?key=3gzH9ujcVtSOqpBTleJY',
+  leafletTileSize: 512,
+  leafletZoomOffset: -1,
+}
