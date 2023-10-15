a3cfa3e5c88954e44c8b5aa60b5a7cdbbb3f0acb
David Straub <straub@protonmail.com>
2023-10-15 10:07:59 +0200

    Bump min API version to 1.3

--- src/GrampsJs.js
+++ src/GrampsJs.js
@@ -89,7 +89,7 @@ const LOADING_STATE_READY = 10
 
 const BASE_DIR = ''
 
-const MINIMUM_API_VERSION = '1.0.0'
+const MINIMUM_API_VERSION = '1.3.0'
 
 export class GrampsJs extends LitElement {
   static get properties() {
