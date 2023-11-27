e897b59ae1a36b0c832c790eb5da57a9b1be6d67
David Straub <straub@protonmail.com>
2023-11-27 22:35:34 +0100

    Refactor charts and address default zoom (#318)

--- src/components/GrampsjsFanChart.js
+++ src/components/GrampsjsFanChart.js
@@ -1,28 +1,16 @@
-import {html, css, LitElement} from 'lit'
+import {html, css} from 'lit'
 
-import {sharedStyles} from '../SharedStyles.js'
-import {GrampsjsTranslateMixin} from '../mixins/GrampsjsTranslateMixin.js'
 import {FanChart} from '../charts/FanChart.js'
+import {GrampsjsChartBase} from './GrampsjsChartBase.js'
 import {getPersonByGrampsId, getTree} from '../charts/util.js'
 
-class GrampsjsFanChart extends GrampsjsTranslateMixin(LitElement) {
+class GrampsjsFanChart extends GrampsjsChartBase {
   static get styles() {
     return [
-      sharedStyles,
+      super.styles,
       css`
         svg a {
           text-decoration: none !important;
-        }
-
-        svg {
-          width: 100%;
-          height: calc(100vh - 64px);
-        }
-
-        div#container {
-          display: flex;
-          margin-left: -40px;
-          margin-right: -40px;
         }
       `,
     ]
@@ -32,7 +20,6 @@ class GrampsjsFanChart extends GrampsjsTranslateMixin(LitElement) {
     return {
       grampsId: {type: String},
       depth: {type: Number},
-      data: {type: Array},
     }
   }
 
@@ -40,17 +27,12 @@ class GrampsjsFanChart extends GrampsjsTranslateMixin(LitElement) {
     super()
     this.grampsId = ''
     this.depth = 5
-    this.data = []
-  }
-
-  render() {
-    if (this.data.length === 0 || !this.grampsId) {
-      return ''
-    }
-    return html`${this.renderChart()}`
   }
 
   renderChart() {
+    if (this.data.length === 0 || !this.grampsId) {
+      return ''
+    }
     const {handle} = getPersonByGrampsId(this.data, this.grampsId)
     if (!handle) {
       return ''
@@ -58,12 +40,12 @@ class GrampsjsFanChart extends GrampsjsTranslateMixin(LitElement) {
     const data = getTree(this.data, handle, this.depth)
     const arcRadius = 60
     return html`
-      <div id="container">
-        ${FanChart(data, {
-          depth: this.depth,
-          arcRadius,
-        })}
-      </div>
+      ${FanChart(data, {
+        depth: this.depth,
+        arcRadius,
+        bboxWidth: this.containerWidth,
+        bboxHeight: this.containerHeight,
+      })}
     `
   }
 }
