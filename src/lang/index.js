afb09a788200ed45576c95b1505fe83745b55cf4
Yusuf Khasbulatov <khashashin@gmail.com>
2024-03-31 01:20:24 +0100

    replace the import assertion with import attributes "with"

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,49 +1,49 @@
 /* eslint-disable camelcase */
-import ar from './ar.json' assert {type: 'json'}
-import bg from './bg.json' assert {type: 'json'}
-import br from './br.json' assert {type: 'json'}
-import ca from './ca.json' assert {type: 'json'}
-import cs from './cs.json' assert {type: 'json'}
-import da from './da.json' assert {type: 'json'}
-import de_AT from './de_AT.json' assert {type: 'json'}
-import de from './de.json' assert {type: 'json'}
-import el from './el.json' assert {type: 'json'}
-import en_GB from './en_GB.json' assert {type: 'json'}
-import en from './en.json' assert {type: 'json'}
-import eo from './eo.json' assert {type: 'json'}
-import es from './es.json' assert {type: 'json'}
-import fi from './fi.json' assert {type: 'json'}
-import fr from './fr.json' assert {type: 'json'}
-import ga from './ga.json' assert {type: 'json'}
-import he from './he.json' assert {type: 'json'}
-import hr from './hr.json' assert {type: 'json'}
-import hu from './hu.json' assert {type: 'json'}
-import is from './is.json' assert {type: 'json'}
-import it from './it.json' assert {type: 'json'}
-import ja from './ja.json' assert {type: 'json'}
-import lt from './lt.json' assert {type: 'json'}
-import lv from './lv.json' assert {type: 'json'}
-import mk from './mk.json' assert {type: 'json'}
-import nb from './nb.json' assert {type: 'json'}
-import nl from './nl.json' assert {type: 'json'}
-import nn from './nn.json' assert {type: 'json'}
-import pl from './pl.json' assert {type: 'json'}
-import pt_BR from './pt_BR.json' assert {type: 'json'}
-import pt_PT from './pt_PT.json' assert {type: 'json'}
-import ro from './ro.json' assert {type: 'json'}
-import ru from './ru.json' assert {type: 'json'}
-import sk from './sk.json' assert {type: 'json'}
-import sl from './sl.json' assert {type: 'json'}
-import sq from './sq.json' assert {type: 'json'}
-import sr from './sr.json' assert {type: 'json'}
-import sv from './sv.json' assert {type: 'json'}
-import ta from './ta.json' assert {type: 'json'}
-import tr from './tr.json' assert {type: 'json'}
-import uk from './uk.json' assert {type: 'json'}
-import vi from './vi.json' assert {type: 'json'}
-import zh_CN from './zh_CN.json' assert {type: 'json'}
-import zh_HK from './zh_HK.json' assert {type: 'json'}
-import zh_TW from './zh_TW.json' assert {type: 'json'}
+import ar from './ar.json' with { type: 'json' }
+import bg from './bg.json' with { type: 'json' }
+import br from './br.json' with { type: 'json' }
+import ca from './ca.json' with { type: 'json' }
+import cs from './cs.json' with { type: 'json' }
+import da from './da.json' with { type: 'json' }
+import de_AT from './de_AT.json' with { type: 'json' }
+import de from './de.json' with { type: 'json' }
+import el from './el.json' with { type: 'json' }
+import en_GB from './en_GB.json' with { type: 'json' }
+import en from './en.json' with { type: 'json' }
+import eo from './eo.json' with { type: 'json' }
+import es from './es.json' with { type: 'json' }
+import fi from './fi.json' with { type: 'json' }
+import fr from './fr.json' with { type: 'json' }
+import ga from './ga.json' with { type: 'json' }
+import he from './he.json' with { type: 'json' }
+import hr from './hr.json' with { type: 'json' }
+import hu from './hu.json' with { type: 'json' }
+import is from './is.json' with { type: 'json' }
+import it from './it.json' with { type: 'json' }
+import ja from './ja.json' with { type: 'json' }
+import lt from './lt.json' with { type: 'json' }
+import lv from './lv.json' with { type: 'json' }
+import mk from './mk.json' with { type: 'json' }
+import nb from './nb.json' with { type: 'json' }
+import nl from './nl.json' with { type: 'json' }
+import nn from './nn.json' with { type: 'json' }
+import pl from './pl.json' with { type: 'json' }
+import pt_BR from './pt_BR.json' with { type: 'json' }
+import pt_PT from './pt_PT.json' with { type: 'json' }
+import ro from './ro.json' with { type: 'json' }
+import ru from './ru.json' with { type: 'json' }
+import sk from './sk.json' with { type: 'json' }
+import sl from './sl.json' with { type: 'json' }
+import sq from './sq.json' with { type: 'json' }
+import sr from './sr.json' with { type: 'json' }
+import sv from './sv.json' with { type: 'json' }
+import ta from './ta.json' with { type: 'json' }
+import tr from './tr.json' with { type: 'json' }
+import uk from './uk.json' with { type: 'json' }
+import vi from './vi.json' with { type: 'json' }
+import zh_CN from './zh_CN.json' with { type: 'json' }
+import zh_HK from './zh_HK.json' with { type: 'json' }
+import zh_TW from './zh_TW.json' with { type: 'json' }
 
 const languages = {
   ar,
