3bdd092d900eaaec99020f4cb81109b5f783a98b
David Straub <straub@protonmail.com>
2025-01-22 22:04:26 +0100

    Finalize match editing

--- src/components/GrampsjsFormEditMatch.js
+++ src/components/GrampsjsFormEditMatch.js
@@ -2,6 +2,8 @@ import {html, css, LitElement} from 'lit'
 import {GrampsjsEditMatchMixin} from '../mixins/GrampsjsEditMatchMixin.js'
 import {GrampsjsTranslateMixin} from '../mixins/GrampsjsTranslateMixin.js'
 import {sharedStyles} from '../SharedStyles.js'
+import {apiGet, apiPut} from '../api.js'
+import {fireEvent} from '../util.js'
 
 export class GrampsjsFormEditMatch extends GrampsjsEditMatchMixin(
   GrampsjsTranslateMixin(LitElement)
@@ -32,6 +34,35 @@ export class GrampsjsFormEditMatch extends GrampsjsEditMatchMixin(
   _handleFormData(e) {
     this.data = {...this.data, raw_data: [e.detail.data]}
     this.checkFormValidity()
+  }
+
+  _handleSaveButton() {
+    this._saveNote()
+    fireEvent(this, 'edit-mode:off')
+  }
+
+  async _saveNote() {
+    const noteText = this.data.raw_data?.[0]
+    const noteHandle = this.data.note_handles?.[0]
+    if (!noteText || !noteHandle) return
+    const noteData = await apiGet(`/api/notes/${noteHandle}`)
+    if ('error' in noteData) {
+      console.error(noteData.error)
+      return
+    }
+    const note = {
+      ...noteData.data,
+      text: {_class: 'StyledText', string: noteText, tags: []},
+    }
+    const data = await apiPut(`/api/notes/${noteHandle}`, note)
+    if ('error' in data) {
+      console.error(data.error)
+    }
+  }
+
+  connectedCallback() {
+    super.connectedCallback()
+    window.addEventListener('edit-mode:save', this._handleSaveButton.bind(this))
   }
 }
 
