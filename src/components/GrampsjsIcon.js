d5232dbc20a9112d03d7053e645e5cd1695b35fa
David Straub <straub@protonmail.com>
2025-06-10 19:43:57 +0000

    Add icon element

---
+++ src/components/GrampsjsIcon.js
@@ -0,0 +1,58 @@
+import {LitElement, html, css} from 'lit'
+import {sharedStyles} from '../SharedStyles.js'
+
+export class GrampsjsIcon extends LitElement {
+  static get styles() {
+    return [
+      sharedStyles,
+      css`
+        :host {
+          display: inline-flex;
+          vertical-align: middle;
+          align-items: center;
+          justify-content: center;
+        }
+
+        svg {
+          display: block;
+        }
+      `,
+    ]
+  }
+
+  static get properties() {
+    return {
+      path: {type: String},
+      color: {type: String},
+      rotate: {type: Number},
+      slot: {type: String},
+      height: {type: Number},
+      width: {type: Number},
+    }
+  }
+
+  constructor() {
+    super()
+    this.path = ''
+    this.color = '#999999'
+    this.rotate = 0
+    this.height = 24
+    this.width = 24
+  }
+
+  render() {
+    return html`
+      <svg
+        height="${this.height}"
+        width="${this.width}"
+        viewBox="0 0 24 24"
+        transform="rotate(${this.rotate})"
+        preserveAspectRatio="xMidYMid meet"
+      >
+        <path fill="${this.color}" d="${this.path}" />
+      </svg>
+    `
+  }
+}
+
+window.customElements.define('grampsjs-icon', GrampsjsIcon)
