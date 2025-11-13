01c2a98ef8a30158a8c2ae124bfe0ec199b4374d
Owen Voke <development@voke.dev>
2025-11-13 08:17:25 +0000

    feat: update to use `md-` buttons

--- src/views/GrampsjsViewHelp.js
+++ src/views/GrampsjsViewHelp.js
@@ -1,40 +1,64 @@
-import {html} from 'lit'
+import {css, html} from 'lit'
 
+import {mdiOpenInNew} from '@mdi/js'
 import {GrampsjsView} from './GrampsjsView.js'
 
 export class GrampsjsViewHelp extends GrampsjsView {
+  static get styles() {
+    return [
+      super.styles,
+      css`
+        .button-container {
+          display: flex;
+          gap: 0.5em;
+          margin-top: 1em;
+          margin-bottom: 0.5em;
+          --button-height: 48px;
+        }
+      `,
+    ]
+  }
+
   renderContent() {
     return html`
       <h2>${this._('Help')}</h2>
 
       <div>
-        <mwc-button
-          outlined
-          icon="arrow_outward"
-          @click="${() =>
-            window.open('https://www.grampsweb.org/user-guide', '_blank')}"
-          >${this._('User Documentation')}</mwc-button
+        <md-outlined-button
+          href="https://www.grampsweb.org/user-guide"
+          target="_blank"
         >
-        <mwc-button
-          outlined
-          icon="arrow_outward"
-          @click="${() =>
-            window.open(
-              'https://www.grampsweb.org/administration/admin',
-              '_blank'
-            )}"
-          >${this._('Administrator Documentation')}</mwc-button
+          ${this._('User Documentation')}
+          <grampsjs-icon
+            .path="${mdiOpenInNew}"
+            slot="icon"
+            color="var(--mdc-theme-primary)"
+          ></grampsjs-icon>
+        </md-outlined-button>
+
+        <md-outlined-button
+          href="https://www.grampsweb.org/administration/admin"
+          target="_blank"
         >
-        <mwc-button
-          outlined
-          icon="arrow_outward"
-          @click="${() =>
-            window.open(
-              'https://gramps.discourse.group/c/gramps-web',
-              '_blank'
-            )}"
-          >${this._('Forum')}</mwc-button
+          ${this._('Administrator Documentation')}
+          <grampsjs-icon
+            .path="${mdiOpenInNew}"
+            slot="icon"
+            color="var(--mdc-theme-primary)"
+          ></grampsjs-icon>
+        </md-outlined-button>
+
+        <md-outlined-button
+          href="https://gramps.discourse.group/c/gramps-web"
+          target="_blank"
         >
+          ${this._('Forum')}
+          <grampsjs-icon
+            .path="${mdiOpenInNew}"
+            slot="icon"
+            color="var(--mdc-theme-primary)"
+          ></grampsjs-icon>
+        </md-outlined-button>
       </div>
     `
   }
