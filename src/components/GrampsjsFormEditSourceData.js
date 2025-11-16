1a036c44cfafadbac8bac7d83da4f1c7a71785e1
Jittymol Mathew <mathewjittymol@gmail.com>
2025-11-16 14:53:29 +0000

    Updated to edit the meta infor of sources - Abbrevation, Author & Public nfo

--- src/components/GrampsjsFormEditSourceData.js
+++ src/components/GrampsjsFormEditSourceData.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
 Form for updating the propertoies of Source - Abbrevation, Author & Pub Info
 */
@@ -48,3 +49,66 @@ window.customElements.define(
   'grampsjs-form-edit-source-data',
   GrampsjsFormEditSourceData
 )
+=======
+/*
+Form for adding a new event reference
+*/
+
+import {html} from 'lit'
+
+import './GrampsjsFormString.js'
+import {GrampsjsObjectForm} from './GrampsjsObjectForm.js'
+
+class GrampsjsFormEditSourceData extends GrampsjsObjectForm {
+  static get properties() {
+    return {
+      prop: {type: Object},
+    }
+  }
+
+  constructor() {
+    super()
+    this.prop = {abbrev: 'abbrev', author: 'author', pubinfo: 'pubinfo'}
+  }
+
+  renderForm() {
+    return html`
+      <div style="width:100%;float:left;margin-bottom:20px;">
+        <grampsjs-form-string
+          @formdata:changed="${this._handleFormData}"
+          fullwidth
+          id="${this.prop?.abbrev}"
+          label="${this._('Abbrevation')}"
+          value="${this.data?.[this.prop?.abbrev] || 'Abbr'}"
+        >
+        </grampsjs-form-string>
+      </div>
+      <div style="width:100%;float:left;margin-bottom:20px;">
+        <grampsjs-form-string
+          @formdata:changed="${this._handleFormData}"
+          fullwidth
+          id="${this.prop?.author}"
+          label="${this._('Author')}"
+          value="${this.data?.[this.prop?.author] || 'auth'}"
+        >
+        </grampsjs-form-string>
+      </div>
+      <div style="width:100%;float:left;margin-bottom:20px;">
+        <grampsjs-form-string
+          @formdata:changed="${this._handleFormData}"
+          fullwidth
+          id="${this.prop?.pubinfo}"
+          label="${this._('Publication Info')}"
+          value="${this.data?.[this.prop?.pubinfo] || 'hgh'}"
+        >
+        </grampsjs-form-string>
+      </div>
+    `
+  }
+}
+
+window.customElements.define(
+  'grampsjs-form-edit-source-data',
+  GrampsjsFormEditSourceData
+)
+>>>>>>> 871c92b (Updated to edit the meta infor of sources - Abbrevation, Author & Public nfo)
--- src/components/GrampsjsSource.js
+++ src/components/GrampsjsSource.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {html} from 'lit'
 
 import '@material/mwc-icon'
@@ -145,3 +146,138 @@ export class GrampsjsSource extends GrampsjsObject {
 }
 
 window.customElements.define('grampsjs-source', GrampsjsSource)
+=======
+import {html} from 'lit'
+
+import '@material/mwc-icon'
+import '@material/web/button/outlined-button'
+
+import {GrampsjsObject} from './GrampsjsObject.js'
+import './GrampsjsFormEditTitle.js'
+import './GrampsjsFormEditSourceData.js'
+import {fireEvent} from '../util.js'
+
+export class GrampsjsSource extends GrampsjsObject {
+  constructor() {
+    super()
+    this._showReferences = false
+    this._objectsName = 'Sources'
+    this._objectEndpoint = 'sources'
+    this._objectIcon = 'bookmarks'
+  }
+
+  renderProfile() {
+    return html`
+      <h2>
+        ${this.data.title || this._('Media Object')}
+        ${this.edit
+          ? html`
+              <mwc-icon-button
+                icon="edit"
+                class="edit"
+                @click="${this._handleEditTitle}"
+              ></mwc-icon-button>
+            `
+          : ''}
+      </h2>
+
+      <dl>
+        ${this.data?.abbrev
+          ? html`
+              <div>
+                <dt>${this._('Abbreviation')}</dt>
+                <dd>${this.data.abbrev}</dd>
+              </div>
+            `
+          : ''}
+        ${this.data?.author
+          ? html`
+              <div>
+                <dt>${this._('Author test')}</dt>
+                <dd>${this.data.author}</dd>
+              </div>
+            `
+          : ''}
+        ${this.data?.pubinfo
+          ? html`
+              <div>
+                <dt>${this._('Publication info')}</dt>
+                <dd>${this.data.pubinfo}</dd>
+              </div>
+            `
+          : ''}
+      </dl>
+      ${this._renderBlogBtn()}
+      ${this.edit
+        ? html`
+            <mwc-icon-button
+              icon="edit"
+              class="edit"
+              @click="${this._handleSourceData}"
+            ></mwc-icon-button>
+          `
+        : ''}
+    `
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  renderPicture() {
+    return ''
+  }
+
+  _renderBlogBtn() {
+    const tags = this.data?.extended?.tags || []
+    if (!tags.filter(tag => tag.name === 'Blog').length > 0) {
+      return ''
+    }
+    return html` <p style="clear: both; padding-top: 1em;">
+      <md-outlined-button @click="${this._handleButtonClick}">
+        ${this._('Show in blog')}
+      </md-outlined-button>
+    </p>`
+  }
+
+  _handleButtonClick() {
+    fireEvent(this, 'nav', {path: `blog/${this.data.gramps_id}`})
+  }
+
+  _handleEditTitle() {
+    this.dialogContent = html`
+      <grampsjs-form-edit-title
+        @object:save="${this._handleSaveTitle}"
+        @object:cancel="${this._handleCancelDialog}"
+        .appState="${this.appState}"
+        .data=${{title: this.data?.title || ''}}
+        prop="title"
+      >
+      </grampsjs-form-edit-title>
+    `
+  }
+
+  _handleSaveTitle(e) {
+    fireEvent(this, 'edit:action', {action: 'updateProp', data: e.detail.data})
+    e.preventDefault()
+    e.stopPropagation()
+    this.dialogContent = ''
+  }
+
+  _handleSourceData() {
+    const data = {
+      abbrev: this.data?.abbrev,
+      author: this.data?.author,
+      pubinfo: this.data?.pubinfo,
+    }
+    this.dialogContent = html`
+      <grampsjs-form-edit-source-data
+        @object:save="${this._handleSaveTitle}"
+        @object:cancel="${this._handleCancelDialog}"
+        .appState="${this.appState}"
+        .data=${data}
+      >
+      </grampsjs-form-edit-source-data>
+    `
+  }
+}
+
+window.customElements.define('grampsjs-source', GrampsjsSource)
+>>>>>>> 871c92b (Updated to edit the meta infor of sources - Abbrevation, Author & Public nfo)
