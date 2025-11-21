8084cd68d489a6f530538046f7c6bf8698ac1310
Jittymol Mathew <mathewjittymol@gmail.com>
2025-11-21 11:01:08 +0100

    Edit Repository Type (#835)
    * Updated for source file changes
    
    * Updated to edit the meta infor of sources - Abbrevation, Author & Public nfo
    
    * Removed unwanted text
    
    * Updated the PR comments
    
    * Updated for edit repository type
    
    * Updated for adding comments on top of the page
    
    * Updated for remove unwanted import

---
+++ src/components/GrampsjsFormEditRepositoryType.js
@@ -0,0 +1,50 @@
+/*
+Form for edit the repository type
+*/
+
+import {html} from 'lit'
+
+import './GrampsjsFormSelectType.js'
+import {GrampsjsObjectForm} from './GrampsjsObjectForm.js'
+
+class GrampsjsFormEditRepositoryType extends GrampsjsObjectForm {
+  constructor() {
+    super()
+    this.isFormValid = true
+  }
+
+  renderForm() {
+    return html`
+      <div>
+        <grampsjs-form-select-type
+          @formdata:changed="${this._handleSelectNewValue}"
+          required
+          id="repository-type"
+          .appState="${this.appState}"
+          ?loadingTypes="${this.loadingTypes}"
+          typeName="repository_types"
+          value=${this.data.type}
+          .types="${this.types}"
+          .typesLocale="${this.typesLocale}"
+        >
+        </grampsjs-form-select-type>
+      </div>
+    `
+  }
+
+  _handleSelectNewValue(e) {
+    if (e.target.value !== '') {
+      this.isFormValid = true
+      this.data.type = e.target.value
+    } else this.isFormValid = false
+  }
+
+  get isValid() {
+    return this.isFormValid
+  }
+}
+
+window.customElements.define(
+  'grampsjs-form-edit-repository-type',
+  GrampsjsFormEditRepositoryType
+)
