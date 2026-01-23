e981d494d341bac4af955526a759b5d7651ee7b6
Satria <Shaker1978@gmx.net>
2026-01-23 08:51:48 +0100

    Chart settings are persisted in the browser's Local Storage (#911)
    * Changed chart defaults.
    
    * Persist chart settings in browser.
    
    * Bugfix: GrampsjsFormSelectType in New-Dialogs did not reset its previous value, when opening the New-Dialog again.
    
    * Fixed review findings.
    
    * Refactoring of chart default values and property handling.
    
    * Fixed a typo

--- src/views/GrampsjsViewHourglassChart.js
+++ src/views/GrampsjsViewHourglassChart.js
@@ -6,15 +6,45 @@ import '../components/GrampsjsTreeChart.js'
 export class GrampsjsViewHourglassChart extends GrampsjsViewTreeChartBase {
   constructor() {
     super()
-    this.nAnc = 2
-    this.nDesc = 1
     this._setAnc = true
     this._setDesc = true
+    this.defaults.nAnc = 2
+  }
+
+  get nAnc() {
+    return this.appState?.settings?.hourglassChartAnc ?? this.defaults.nAnc
+  }
+
+  set nAnc(value) {
+    this.appState.updateSettings({hourglassChartAnc: value}, false)
+  }
+
+  get nDesc() {
+    return this.appState?.settings?.hourglassChartDesc ?? this.defaults.nDesc
+  }
+
+  set nDesc(value) {
+    this.appState.updateSettings({hourglassChartDesc: value}, false)
+  }
+
+  get nameDisplayFormat() {
+    return (
+      this.appState?.settings?.hourglassChartNameDisplayFormat ??
+      this.defaults.nameDisplayFormat
+    )
+  }
+
+  set nameDisplayFormat(value) {
+    this.appState.updateSettings(
+      {hourglassChartNameDisplayFormat: value},
+      false
+    )
   }
 
   _resetLevels() {
-    this.nAnc = 2
-    this.nDesc = 1
+    this.nAnc = this.defaults.nAnc
+    this.nDesc = this.defaults.nDesc
+    this.nameDisplayFormat = this.defaults.nameDisplayFormat
   }
 
   renderChart() {
