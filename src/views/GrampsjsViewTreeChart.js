4e074a37f3e2176e21d7d516c29873dc118ccb60
Jittymol Mathew <mathewjittymol@gmail.com>
2026-03-29 18:00:40 +0000

    Updated for UI for add family member popup

--- src/views/GrampsjsViewTreeChart.js
+++ src/views/GrampsjsViewTreeChart.js
@@ -1,31 +1,24 @@
 import {html} from 'lit'
-import '@material/web/dialog/dialog.js'
-import '@material/web/button/text-button.js'
-import '@material/web/list/list.js'
-import '@material/web/list/list-item.js'
 import {GrampsjsViewTreeChartBase} from './GrampsjsViewTreeChartBase.js'
-import {fireEvent} from '../util.js'
 import '../components/GrampsjsTreeChart.js'
-import '../components/GrampsjsFormNewPerson.js'
+import '../components/GrampsjsTreeChartAddPerson.js'
 
 export class GrampsjsViewTreeChart extends GrampsjsViewTreeChartBase {
-  static get properties() {
-    return {
-      _addPersonRelationDialogOpen: {type: Boolean},
-      _addPersonRelationDialogData: {type: Object},
-      _newPersonFormDialogOpen: {type: Boolean},
-      relationship: {type: String},
-    }
-  }
-
   constructor() {
     super()
     this._setAnc = true
     this.defaults.nAnc = 3
-    this._addPersonRelationDialogOpen = false
-    this._addPersonRelationDialogData = {}
-    this._newPersonFormDialogOpen = false
-    this.relationship = ''
+  }
+
+  connectedCallback() {
+    super.connectedCallback()
+    this._boundHandleDbChanged = () => this._fetchData(this.grampsId)
+    window.addEventListener('db:changed', this._boundHandleDbChanged)
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback()
+    window.removeEventListener('db:changed', this._boundHandleDbChanged)
   }
 
   get nAnc() {
@@ -52,102 +45,18 @@ export class GrampsjsViewTreeChart extends GrampsjsViewTreeChartBase {
     this.nameDisplayFormat = this.defaults.nameDisplayFormat
   }
 
-  _selectRelationship(relationship) {
-    this.relationship = relationship
-    this._addPersonRelationDialogOpen = false
-    this._newPersonFormDialogOpen = true
-  }
-
-  async _handleNewPersonSave(e) {
-    const {processedData} = e.detail.data
-
-    if (!['father', 'mother', 'child'].includes(this.relationship)) {
-      this._newPersonFormDialogOpen = false
-      return
-    }
-
-    // TODO: Update the family relationship for the selected relation after
-    // creating the new person. The current implementation only creates the
-    // person record and still needs the family linkage logic.
-    const createPerson = await this.appState.apiPost(
-      '/api/objects/',
-      processedData
+  _handleAddPersonRelation(e) {
+    const addPersonEl = this.renderRoot.querySelector(
+      'grampsjs-tree-chart-add-person'
     )
-
-    if ('error' in createPerson) {
-      fireEvent(this, 'grampsjs:error', {message: createPerson.error})
-      return
+    if (addPersonEl) {
+      addPersonEl.open(e.detail.data)
     }
-
-    e.preventDefault()
-    e.stopPropagation()
-    this._newPersonFormDialogOpen = false
-    this._addPersonRelationDialogData = {}
-    this.relationship = ''
-    this._fetchData(this.grampsId)
-  }
-
-  renderAddPersonRelationDialog() {
-    return html`
-      <md-dialog
-        ?open="${this._addPersonRelationDialogOpen}"
-        @close="${() => {
-          this._addPersonRelationDialogOpen = false
-          this._addPersonRelationDialogData = {}
-        }}"
-      >
-        <div slot="headline">${this._('Relationship')}</div>
-        <md-list slot="content">
-          <md-list-item
-            type="button"
-            @click="${() => this._selectRelationship('father')}"
-          >
-            ${this._('Father')}
-          </md-list-item>
-          <md-list-item
-            type="button"
-            @click="${() => this._selectRelationship('mother')}"
-          >
-            ${this._('Mother')}
-          </md-list-item>
-          <md-list-item
-            type="button"
-            @click="${() => this._selectRelationship('child')}"
-          >
-            ${this._('Child')}
-          </md-list-item>
-        </md-list>
-      </md-dialog>
-    `
-  }
-
-  _renderNewPersonDialog() {
-    if (!this._newPersonFormDialogOpen) {
-      return ''
-    }
-
-    return html`
-      <grampsjs-form-new-person
-        @object:save="${this._handleNewPersonSave}"
-        @object:cancel="${() => {
-          this._newPersonFormDialogOpen = false
-          this.relationship = ''
-        }}"
-        .appState="${this.appState}"
-        dialogTitle="${this._('Add a new person')}"
-      >
-      </grampsjs-form-new-person>
-    `
   }
 
   renderChart() {
     return html`
-      <div
-        @add-new-person-relation="${e => {
-          this._addPersonRelationDialogOpen = true
-          this._addPersonRelationDialogData = e.detail.data
-        }}"
-      >
+      <div @add-new-person-relation="${this._handleAddPersonRelation}">
         <grampsjs-tree-chart
           ancestors
           grampsId=${this.grampsId}
@@ -165,8 +74,10 @@ export class GrampsjsViewTreeChart extends GrampsjsViewTreeChartBase {
 
   renderContent() {
     return html`
-      ${super.renderContent()} ${this.renderAddPersonRelationDialog()}
-      ${this._renderNewPersonDialog()}
+      ${super.renderContent()}
+      <grampsjs-tree-chart-add-person
+        .appState="${this.appState}"
+      ></grampsjs-tree-chart-add-person>
     `
   }
 }
