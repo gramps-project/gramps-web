e56bd18b736b937bc1de1ccc4aed5e8503beecb6
David Straub <straub@protonmail.com>
2026-04-14 12:54:29 +0200

    Modernize components in search filter, change some icons (#1085)

--- src/components/GrampsjsButtonToggle.js
+++ src/components/GrampsjsButtonToggle.js
@@ -1,6 +1,6 @@
 import {LitElement, css, html} from 'lit'
-import '@material/mwc-button'
-import '@material/mwc-icon'
+import '@material/web/chips/filter-chip'
+import './GrampsjsIcon.js'
 
 import {sharedStyles} from '../SharedStyles.js'
 import {GrampsjsAppStateMixin} from '../mixins/GrampsjsAppStateMixin.js'
@@ -11,11 +11,34 @@ export class GrampsjsButtonToggle extends GrampsjsAppStateMixin(LitElement) {
     return [
       sharedStyles,
       css`
-        mwc-button {
-          --mdc-typography-button-text-transform: none;
-          --mdc-typography-button-font-weight: 400;
-          --mdc-typography-button-letter-spacing: 0px;
-          --mdc-typography-button-font-size: 13px;
+        md-filter-chip {
+          --md-filter-chip-label-text-size: 13px;
+          --md-filter-chip-label-text-weight: 400;
+          --md-filter-chip-outline-width: 0px;
+          --md-filter-chip-label-text-color: var(--md-sys-color-primary);
+          --md-filter-chip-hover-label-text-color: var(--md-sys-color-primary);
+          --md-filter-chip-focus-label-text-color: var(--md-sys-color-primary);
+          --md-filter-chip-pressed-label-text-color: var(
+            --md-sys-color-primary
+          );
+          --md-filter-chip-selected-container-color: var(
+            --md-sys-color-primary
+          );
+          --md-filter-chip-selected-label-text-color: var(
+            --md-sys-color-on-primary
+          );
+          --md-filter-chip-selected-hover-label-text-color: var(
+            --md-sys-color-on-primary
+          );
+          --md-filter-chip-selected-focus-label-text-color: var(
+            --md-sys-color-on-primary
+          );
+          --md-filter-chip-selected-pressed-label-text-color: var(
+            --md-sys-color-on-primary
+          );
+          --md-filter-chip-selected-hover-state-layer-color: var(
+            --md-sys-color-on-primary
+          );
         }
       `,
     ]
@@ -23,27 +46,45 @@ export class GrampsjsButtonToggle extends GrampsjsAppStateMixin(LitElement) {
 
   static get properties() {
     return {
-      icon: {type: String},
+      iconPath: {type: String},
+      label: {type: String},
       checked: {type: Boolean},
     }
   }
 
   constructor() {
     super()
-    this.icon = ''
+    this.iconPath = ''
+    this.label = ''
     this.checked = false
   }
 
   render() {
     return html`
-      <mwc-button
-        dense
-        ?unelevated="${this.checked}"
-        icon="${this.icon}"
+      <md-filter-chip
+        label="${this.label}"
+        ?selected="${this.checked}"
         @click="${this.toggle}"
       >
-        <slot></slot>
-      </mwc-button>
+        ${this.iconPath
+          ? html`
+              <grampsjs-icon
+                slot="icon"
+                path="${this.iconPath}"
+                color="var(--md-sys-color-primary)"
+                height="18"
+                width="18"
+              ></grampsjs-icon>
+              <grampsjs-icon
+                slot="selected-icon"
+                path="${this.iconPath}"
+                color="var(--md-sys-color-on-primary)"
+                height="18"
+                width="18"
+              ></grampsjs-icon>
+            `
+          : ''}
+      </md-filter-chip>
     `
   }
 
