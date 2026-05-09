8cf9b6897e5669843e5494d5467b184b62bb0305
David Straub <straub@protonmail.com>
2026-05-09 15:38:16 +0200

    Fixes to family view parent edit buttons (#1140)

--- src/components/GrampsjsFamily.js
+++ src/components/GrampsjsFamily.js
@@ -23,6 +23,22 @@ export class GrampsjsFamily extends GrampsjsObject {
             font-weight: bold;
             color: var(--grampsjs-body-font-color-35);
           }
+
+          .parent-row {
+            display: flex;
+            align-items: center;
+            gap: 0;
+          }
+
+          .parent-info {
+            flex: 1;
+          }
+
+          .parent-actions {
+            display: flex;
+            align-items: center;
+            flex-shrink: 0;
+          }
         }
       `,
     ]
@@ -59,15 +75,6 @@ export class GrampsjsFamily extends GrampsjsObject {
       ${this._renderParent('father', this._parentLabel('father'))}
       ${this._renderParent('mother', this._parentLabel('mother'))}
       ${this._renderMarriageBlock()}
-      ${this.edit
-        ? html`
-            <mwc-icon-button
-              icon="edit"
-              class="edit"
-              @click="${this._handleEditFamily}"
-            ></mwc-icon-button>
-          `
-        : ''}
     `
   }
 
@@ -86,30 +93,45 @@ export class GrampsjsFamily extends GrampsjsObject {
     const divorce = this.data?.profile?.divorce
     const hasMarriage = marriage?.date || marriage?.place
     const hasDivorce = divorce && Object.keys(divorce).length > 0
-    if (!relType && !hasMarriage && !hasDivorce) {
+    if (!relType && !hasMarriage && !hasDivorce && !this.edit) {
       return ''
     }
     return html`
       <dl>
         <div>
           <dt>${this._('Relationship type:').replace(':', '')}</dt>
-          <dd>
-            ${relType || ''}
-            ${hasMarriage
-              ? html`<span class="parent-dates">
-                  <span class="sym">⚭</span>
-                  ${marriage.date || ''}
-                  ${marriage.place
-                    ? `${this._('in')} ${marriage.place_name || marriage.place}`
-                    : ''}
-                </span>`
-              : ''}
-            ${hasDivorce
-              ? html`<span class="parent-dates">
-                  <span class="sym">⚮</span>
-                  ${divorce.date || ''}
-                  ${divorce.place ? `${this._('in')} ${divorce.place}` : ''}
-                </span>`
+          <dd class="${this.edit ? 'parent-row' : ''}">
+            <div class="parent-info">
+              ${relType || ''}
+              ${hasMarriage
+                ? html`<span class="parent-dates">
+                    <span class="sym">⚭</span>
+                    ${marriage.date || ''}
+                    ${marriage.place
+                      ? `${this._('in')} ${
+                          marriage.place_name || marriage.place
+                        }`
+                      : ''}
+                  </span>`
+                : ''}
+              ${hasDivorce
+                ? html`<span class="parent-dates">
+                    <span class="sym">⚮</span>
+                    ${divorce.date || ''}
+                    ${divorce.place ? `${this._('in')} ${divorce.place}` : ''}
+                  </span>`
+                : ''}
+            </div>
+            ${this.edit
+              ? html`
+                  <div class="parent-actions">
+                    <mwc-icon-button
+                      icon="edit"
+                      class="edit"
+                      @click="${this._handleEditFamily}"
+                    ></mwc-icon-button>
+                  </div>
+                `
               : ''}
           </dd>
         </div>
@@ -127,45 +149,53 @@ export class GrampsjsFamily extends GrampsjsObject {
       <dl>
         <div>
           <dt>${label}</dt>
-          <dd>
-            ${hasProfile
-              ? html`<a href="/person/${profile.gramps_id}"
-                    >${profile.name_given || '…'}
-                    ${profile.name_surname || '…'}</a
-                  >
-                  ${birthDate || deathDate
-                    ? html`<span class="parent-dates">
-                        ${birthDate
-                          ? html`<span class="sym">∗</span> ${birthDate}`
-                          : ''}
-                        ${birthDate && deathDate ? ' ' : ''}
-                        ${deathDate
-                          ? html`<span class="sym">†</span> ${deathDate}`
-                          : ''}
-                      </span>`
-                    : ''}`
-              : '…'}
-            ${this.edit && hasProfile
-              ? html`
-                  <mwc-icon-button
-                    class="edit"
-                    icon="link_off"
-                    @click="${e => this._handleParentChanged(e, parent)}"
-                  ></mwc-icon-button>
-                `
-              : ''}
+          <dd class="${this.edit ? 'parent-row' : ''}">
+            <div class="parent-info">
+              ${hasProfile
+                ? html`<a href="/person/${profile.gramps_id}"
+                      >${profile.name_given || '…'}
+                      ${profile.name_surname || '…'}</a
+                    >
+                    ${birthDate || deathDate
+                      ? html`<span class="parent-dates">
+                          ${birthDate
+                            ? html`<span class="sym">∗</span> ${birthDate}`
+                            : ''}
+                          ${birthDate && deathDate ? ' ' : ''}
+                          ${deathDate
+                            ? html`<span class="sym">†</span> ${deathDate}`
+                            : ''}
+                        </span>`
+                      : ''}`
+                : '…'}
+            </div>
             ${this.edit
               ? html`
-                  <mwc-icon-button
-                    class="edit"
-                    icon="add_link"
-                    @click="${() => this._handleParentShare(parent)}"
-                  ></mwc-icon-button>
-                  <mwc-icon-button
-                    class="edit"
-                    icon="add"
-                    @click="${() => this._handleAddNewParent(parent)}"
-                  ></mwc-icon-button>
+                  <div class="parent-actions">
+                    ${hasProfile
+                      ? html`
+                          <mwc-icon-button
+                            class="edit"
+                            icon="link_off"
+                            title="${this._('Remove')}"
+                            @click="${e =>
+                              this._handleParentChanged(e, parent)}"
+                          ></mwc-icon-button>
+                        `
+                      : ''}
+                    <mwc-icon-button
+                      class="edit"
+                      icon="add_link"
+                      title="${this._('Select an existing person')}"
+                      @click="${() => this._handleParentShare(parent)}"
+                    ></mwc-icon-button>
+                    <mwc-icon-button
+                      class="edit"
+                      icon="add"
+                      title="${this._('Add a new person')}"
+                      @click="${() => this._handleAddNewParent(parent)}"
+                    ></mwc-icon-button>
+                  </div>
                 `
               : ''}
           </dd>
