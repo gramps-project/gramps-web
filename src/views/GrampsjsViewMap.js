dd2e7bae7dbb4161ddbcdccbca351cb3f793b124
David Straub <straub@protonmail.com>
2026-07-27 08:01:17 +0200

    Map overlay respects bounds (#1312)
    * Map overlay respects bounds
    
    * Address comments

--- src/views/GrampsjsViewMap.js
+++ src/views/GrampsjsViewMap.js
@@ -483,10 +483,22 @@ export class GrampsjsViewMap extends GrampsjsStaleDataMixin(GrampsjsView) {
   }
 
   _renderMapLayer(obj) {
+    const boundsAttr = obj.attribute_list?.find(
+      attr => attr.type === 'map:bounds'
+    )?.value
+    let bounds = null
+    if (boundsAttr) {
+      try {
+        bounds = JSON.parse(boundsAttr)
+      } catch {
+        bounds = null
+      }
+    }
     return html`
       <grampsjs-map-tile-layer
         handle="${obj.handle}"
         checksum="${obj.checksum}"
+        .bounds="${bounds}"
         ?hidden="${this._hiddenOverlaysHandles.includes(obj.handle)}"
       ></grampsjs-map-tile-layer>
     `
