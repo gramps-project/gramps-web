be8fb25ee1dddf6da35baa99f1b738ecc999eedf
David Straub <straub@protonmail.com>
2026-09-22 16:31:06 +0000

    Add optional link to cloud account in admin dropdown

--- src/components/GrampsjsSettingsMenu.js
+++ src/components/GrampsjsSettingsMenu.js
@@ -17,6 +17,8 @@ import {
   mdiAccountCog,
   mdiAccountCircle,
   mdiHelp,
+  mdiCloud,
+  mdiOpenInNew,
 } from '@mdi/js'
 import {sharedStyles, appBarIconButtonStyles} from '../SharedStyles.js'
 import {GrampsjsAppStateMixin} from '../mixins/GrampsjsAppStateMixin.js'
@@ -49,6 +51,11 @@ class GrampsjsSettingsMenu extends GrampsjsAppStateMixin(LitElement) {
           --md-menu-item-one-line-container-height: 56px;
           --icon-color: var(--grampsjs-body-font-color-35);
         }
+        md-menu-item md-icon[slot='end'] svg {
+          height: 18px;
+          width: 18px;
+        }
+
         md-menu-item.red {
           --md-menu-item-label-text-color: var(--grampsjs-logout-font-color);
           --icon-color: var(--grampsjs-logout-font-color);
@@ -82,6 +89,7 @@ class GrampsjsSettingsMenu extends GrampsjsAppStateMixin(LitElement) {
           menu-corner="end-end"
         >
         ${menuItems.map(menuItem => this._menuItem(...menuItem))}
+          ${this._renderAccountItem()}
           <md-divider role="separator" tabindex="-1"></md-divider>
           <md-menu-item class="red"
             @click="${() => this.appState.signout()}"
@@ -94,6 +102,33 @@ class GrampsjsSettingsMenu extends GrampsjsAppStateMixin(LitElement) {
             )}</md-icon>
           </md-menu-item>
       </div>
+    `
+  }
+
+  // Link to the page where the provider hosting this instance lets owners manage
+  // it, set via `accountUrl` and `accountName` in config.js. The label is the
+  // provider's name, a proper noun, so it needs no translation. Shown to tree
+  // owners only, since nobody else can act on what it leads to.
+  _renderAccountItem() {
+    const {accountUrl, accountName} = this.appState.frontendConfig
+    if (
+      !accountUrl ||
+      !accountName ||
+      !this.appState.permissions.canManageUsers
+    ) {
+      return ''
+    }
+    return html`
+      <md-divider role="separator" tabindex="-1"></md-divider>
+      <md-menu-item href="${accountUrl}" target="_blank">
+        <div slot="headline">${accountName}</div>
+        <md-icon slot="start"
+          >${renderIconSvg(mdiCloud, 'var(--icon-color)')}</md-icon
+        >
+        <md-icon slot="end"
+          >${renderIconSvg(mdiOpenInNew, 'var(--icon-color)')}</md-icon
+        >
+      </md-menu-item>
     `
   }
 
